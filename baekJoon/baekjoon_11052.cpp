492dd01cd10f8ea34ae11412526011a97b727a99
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-12-28 01:35:29 +0900

    [Silver 1] 카드 구매하기

---
+++ baekJoon/baekjoon_11052.cpp
@@ -0,0 +1,23 @@
+// 카드 구매하기
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[1001];
+int dp[1001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= i; j++)
+			dp[i] = max(dp[i], dp[i - j] + arr[j]);
+	cout << dp[n] << '\n';
+	return 0;
+}
