9bd7f54dad76001d30a9a538cbe8434eb1d60aec
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-12-28 00:20:28 +0900

    [Gold 4] 개미

---
+++ baekJoon/baekjoon_2136.cpp
@@ -0,0 +1,20 @@
+// 개미
+
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+int n, l, a, b, c, pcnt;
+pair<int, int> p[100000];
+int main() {
+    scanf("%d %d", &n, &l);
+    for (int i = 1, x; i <= n; i++) {
+        scanf("%d", &x);
+        if (x<0) c++, a = -x>a ? -x : a;
+        else b = l - x > b ? l - x : b;
+        p[pcnt++] = { abs(x),i };
+    }
+    sort(p, p + pcnt);
+    if (a>b) printf("%d %d", p[c - 1].second, a);
+    else printf("%d %d", p[c].second, b);
+    return 0;
+}
