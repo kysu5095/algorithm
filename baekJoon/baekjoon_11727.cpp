cb307daaed8e4cf4be2333d094b6de06b9e9957d
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2020-12-28 14:20:10 +0900

    [Silver 3] 2xn 타일링 2

---
+++ baekJoon/baekjoon_11727.cpp
@@ -0,0 +1,19 @@
+// 2xn 타일링 2
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[1001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n;
+	arr[0] = arr[1] = 1;
+	for (int i = 2; i <= n; i++) 
+		arr[i] = ((arr[i - 2] * 2) + arr[i - 1]) % 10007;
+	cout << arr[n] << '\n';
+	return 0;
+}
