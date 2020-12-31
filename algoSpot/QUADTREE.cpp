655b3ff9a45e0d0bf75d49c00c9b7270a57729bd
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-01-01 01:51:25 +0900

    [알고리즘 문제 해결 전략] 쿼드 트리 뒤집기

--- algoSpot/QUADTREE.cpp
+++ algoSpot/QUADTREE.cpp
@@ -1,34 +1,35 @@
+// 쿼드 트리 뒤집기
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-//문자열의 반복자를 인자로 사용
-string reverse(string::iterator& it){
-    char head = *it;
-    ++it;
-    //문자열의 첫 문자가 b나 w인 경우(사각형의 색이 같은 경우) 문자 그대로 반환
-    if(head == 'b' || head == 'w')
-        return string(1, head);
-    //인자로 받은 문자열의 첫 번째 문자를 제외하고 나머지 문자열을 통해 재귀 호출
-    string upperLeft = reverse(it);
-    string upperRight = reverse(it);
-    string lowerLeft = reverse(it);
-    string lowerRight = reverse(it);
+int test_case;
+string str;
 
-    //위, 아래 조각들을 반대로 교체한다.
-    return string("x") + lowerLeft + lowerRight + upperLeft + upperRight;
+string dfs(int& idx) {
+	idx++;
+	if (str[idx] == 'b') return "b";
+	if (str[idx] == 'w') return "w";
+	string upLeft = dfs(idx);
+	string upRight = dfs(idx);
+	string downLeft = dfs(idx);
+	string downRight = dfs(idx);
+
+	return "x" + downLeft + downRight + upLeft + upRight;
 }
 
-int main(){
-    string str;
-    int _;
-    cin >> _;
-	while(_--){
-        cin >> str;
-        string::iterator it = str.begin();
-        cout << reverse(it) << endl;
-    }
-	
-    return 0;
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	int idx;
+	cin >> test_case;
+	while (test_case--) {
+		idx = -1;
+		str.clear();
+		cin >> str;
+		cout << dfs(idx) << '\n';
+	}
+	return 0;
 }
