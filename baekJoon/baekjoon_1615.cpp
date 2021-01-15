6bdecb400300f3b47e0d6a2801752ab0209166ce
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-01-15 16:55:54 +0900

    [Platinum 5] 교차개수세기

--- baekJoon/baekjoon_1615.cpp
+++ baekJoon/baekjoon_1615.cpp
@@ -1,46 +1,49 @@
 // 교차개수세기
 
+#define ll long long
+#define MAX 2000
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int n, m;
+ll tree[(MAX << 2) + 1];
 vector<pair<int, int>> v;
-int arr[2001];
 
-void update(int idx, int val) {
-	while (idx <= n) {
-		arr[idx] += val;
-		idx += (idx & -idx);
-	}
+void update(int node, int l, int r, int& idx) {
+	if (idx < l || idx > r) return;
+	tree[node] += 1;
+	if (l == r) return;
+	int next = node << 1;
+	int mid = (l + r) >> 1;
+	update(next, l, mid, idx);
+	update(next + 1, mid + 1, r, idx);
 }
 
-int query(int idx) {
-	int ret = 0;
-	while (idx) {
-		ret += arr[idx];
-		idx -= (idx & -idx);
-	}
-	return ret;
+ll query(int node, int l, int r, int start, int end) {
+	if (l > end || r < start) return 0;
+	if (l >= start && r <= end) return tree[node];
+	int next = node << 1;
+	int mid = (l + r) >> 1;
+	return query(next, l, mid, start, end) + query(next + 1, mid + 1, r, start, end);
 }
 
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
 	int a, b;
+	ll res = 0;
 	cin >> n >> m;
+	v.resize(m);
 	for (int i = 0; i < m; i++) {
-		cin >> a >> b;
-		v.push_back(make_pair(a, b));
+		cin >> v[i].first >> v[i].second;
 	}
 	sort(v.begin(), v.end());
-
-	int res = 0;
 	for (int i = 0; i < m; i++) {
-		res += (query(n) - query(v[i].second));
-		update(v[i].second, 1);
+		res += query(1, 1, n, v[i].second + 1, n);
+		update(1, 1, n, v[i].second);
 	}
 	cout << res << '\n';
 	return 0;
