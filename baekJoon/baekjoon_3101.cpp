8fc956f19948ab541a03c3fa6c5990e9b378d961
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-01-21 02:06:23 +0900

    [Gold 2] 토끼의 이동

--- baekJoon/baekjoon_3101.cpp
+++ baekJoon/baekjoon_3101.cpp
@@ -1,107 +1,42 @@
-#include <cstdio>
+// 토끼의 이동
 
- 
+#include <iostream>
+#define ll long long
 
 using namespace std;
 
- 
+ll N, k;
+char command[300001];
 
-long long Sigma(int N)
-
-{
-
-    return 1LL * N * (N + 1LL) / 2LL;
-
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	ll y = 0, x = 0;
+	ll val, idx, differ, res = 1;
+	cin >> N >> k;
+	cin >> command;
+	for (int i = 0; i < k; i++) {
+		switch (command[i]) {
+			case 'U': y--; break;
+			case 'D': y++; break;
+			case 'R': x++; break;
+			case 'L': x--; break;
+		}
+		differ = (x + y);
+		if (differ < N) {
+			val = (differ * (differ + 1) / 2);
+			if (differ % 2 == 0) val += (x + 1);
+			else val += (y + 1);
+		}
+		else {
+			idx = (N * 2) - (differ + 1);
+			val = (N * N) - ((idx * (idx + 1)) / 2) + 1;
+			if ((x + y) % 2 == 0) val += (N - y - 1);
+			else val += (N - x - 1);
+		}
+		res += val;
+		//cout << command[i] << " (" << y << "," << x << ") :" << val << "(" << res << ")" <<'\n';
+	}
+	cout << res << '\n';
+	return 0;
 }
-
- 
-
-int main()
-
-{
-
-    int N, K, R = 0, C = 0;
-
-    scanf("%d %d\n", &N, &K);
-
-    long long result = 1;
-
-    for (int i = 0; i < K; i++)
-
-    {
-
-        switch (getchar())
-
-        {
-
-        case 'U':
-
-            --R;
-
-            break;
-
-        case 'D':
-
-            ++R;
-
-            break;
-
-        case 'L':
-
-            --C;
-
-            break;
-
-        default:
-
-            ++C;
-
-        }
-
-        
-
-        // 좌표를 이용해서 값을 얻어낼 수 있다.
-
-        long long first, dist;
-
-        if (R + C < N)
-
-        {
-
-            // 각 줄은 1의 등차수열이다.
-
-            // 첫번째 값을 얻어내고
-
-            first = 1LL + Sigma(R + C);
-
-            // 첫번쨰 값의 좌표로부터 얼마나 떨어져 있는지를 계산한다.
-
-            dist = ((R + C) % 2 ? R : C);
-
-        } 
-
-        // 여기를 계산할 때 주의해야 한다.
-
-        else
-
-        {
-
-            first = 1LL + Sigma(N) + Sigma(N - 1) - Sigma(2 * N - (R + C + 1));
-
-            dist = ((R + C) % 2 ? R : C) - ((long long)R + C - N + 1);
-
-        }
-
-        result += (first + dist);
-
-    }
-
- 
-
-    printf("%lld\n", result);
-
-}
-
-
-
-출처: https://haedallog.tistory.com/161 [SM's Development Log]
