9beb875796d53dcaa67b623670f6dc37ededd773
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-02-03 15:36:22 +0900

    [Silver 1] 세훈이의 선물가게

--- baekJoon/baekjoon_17225.cpp
+++ baekJoon/baekjoon_17225.cpp
@@ -1,75 +1,49 @@
 // 세훈이의 선물가게
 
 #include <iostream>
-#include <queue>
 #include <vector>
-#define MAX 90000
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
 int A, B, n;
-queue<pair<int, int>> qa, qb;
-vector<int> va, vb;
+priority_queue<pair<int, char>> pq;
+
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
-	int t, cnt;
+	int t, cnt, t_a = 0, t_b = 0;
 	char ch;
 	cin >> A >> B >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> t >> ch >> cnt;
-		if (ch == 'B') qa.push(make_pair(t, cnt));
-		else qb.push(make_pair(t, cnt));
+		if (ch == 'B') {
+			t = max(t, t_a);
+			for (int i = t, j = 0; j < cnt; i += A, j++)
+				pq.push(make_pair(-i, 'B'));
+			t_a = t + A * cnt;
+		}
+		else {
+			t = max(t, t_b);
+			for (int i = t, j = 0; j < cnt; i += B, j++)
+				pq.push(make_pair(-i, 'A'));
+			t_b = t + B * cnt;
+		}
 	}
-	int idx = 1;
-	int a_t = qa.front().first;
-	int b_t = qb.front().first;
-	int a_cnt = qa.front().second;
-	int b_cnt = qb.front().second;
-	bool a_flag;
-	while (!qa.empty() || !qb.empty()) {
-		a_flag = false;
-		if (a_t <= b_t) {
-			va.push_back(idx++);
-			a_cnt--;
-			a_flag = true;
-		}
-		if (a_t >= b_t) {
-			vb.push_back(idx++);
-			b_t += B;
-			b_cnt--;
-		}
-		if (a_flag) a_t += A;
-		if (!a_cnt && !qa.empty()) {
-			qa.pop();
-			if(qa.empty()){
-				a_t = MAX;
-			}
-			else {
-				if (qa.front().first > a_t)
-					a_t = qa.front().first;
-				a_cnt = qa.front().second;
-			}
-		}
-		if (!b_cnt && !qb.empty()) {
-			qb.pop();
-			if (qb.empty()) {
-				b_t = MAX;
-			}
-			else {
-				if (qb.front().first > b_t)
-					b_t = qb.front().first;
-				b_cnt = qb.front().second;
-			}
-		}
+	cnt = 1;
+	vector<int> va, vb;
+	while (!pq.empty()) {
+		ch = pq.top().second;
+		pq.pop();
+		if (ch == 'B') va.push_back(cnt++);
+		else vb.push_back(cnt++);
 	}
 	cout << va.size() << '\n';
-	for (auto idx : va)
-		cout << idx << " ";
+	for (int idx : va) cout << idx << " ";
 	cout << '\n';
 	cout << vb.size() << '\n';
-	for (auto idx : vb)
-		cout << idx << " ";
+	for (int idx : vb) cout << idx << " ";
 	cout << '\n';
 	return 0;
 }
