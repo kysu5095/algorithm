242b14a37ae325f7bd2c392e9ca3dcf32e03d0b2
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-02-07 05:09:19 +0900

    [Gold 1] 자리 전쟁_실패

--- baekJoon/baekjoon_2886.cpp
+++ baekJoon/baekjoon_2886.cpp
@@ -1,54 +1,18 @@
 // 자리 전쟁
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <string.h>
 #include <math.h>
-#include <limits.h>
-#include <queue>
+#define POW(A, B, C, D) pow(A-C, 2) + pow(B-D, 2)
 
 using namespace std;
 
 int r, c;
 char arr[101][101];
-bool visited[101][101];
-int dis[101][101][2];
-int dy[4] = { 0, 0, 1, -1 };
-int dx[4] = { 1, -1, 0, 0 };
-vector<pair<int, int>> v;
-
-void bfs(int y, int x) {
-	memset(visited, false, sizeof(visited));
-	queue<pair<int, int>> q;
-	q.push(make_pair(y, x));
-	visited[y][x] = true;
-	int ny, nx, len, cnt = 0;
-	while (!q.empty()) {
-		cnt++;
-		len = q.size();
-		while (len--) {
-			y = q.front().first;
-			x = q.front().second;
-			q.pop();
-			for (int i = 0; i < 4; i++) {
-				ny = y + dy[i];
-				nx = x + dx[i];
-				if (ny < 0 || ny == r || nx < 0 || nx == c || visited[ny][nx]) continue;
-				if (arr[ny][nx] == 'L') {
-					if (dis[ny][nx][0] > cnt) {
-						dis[ny][nx][0] = cnt;
-						dis[ny][nx][1] = 1;
-					}
-					else if (dis[ny][nx][0] == cnt) {
-						dis[ny][nx][1]++;
-					}
-				}
-				q.push(make_pair(ny, nx));
-				visited[ny][nx] = true;
-			}
-		}
-	}
-}
+bool map[101][101] = { {false,}, };
+bool visited[10001] = { false, };
+vector<pair<int, int>> chair, person;
 
 int main() {
 	cin.sync_with_stdio(0);
@@ -57,21 +21,41 @@ int main() {
 	for (int i = 0; i < r; i++) {
 		cin >> arr[i];
 		for (int j = 0; j < c; j++) {
-			if (arr[i][j] == 'X')
-				v.push_back(make_pair(i, j));
+			if (arr[i][j] == 'X') person.push_back(make_pair(i, j));
+			else if (arr[i][j] == 'L') chair.push_back(make_pair(i, j));
 		}
 	}
-	for (int i = 0; i < r; i++)
-		for (int j = 0; j < c; j++)
-			dis[i][j][0] = INT_MAX;
-	for (int i = 0; i < v.size(); i++)
-		bfs(v[i].first, v[i].second);
-
-	int res = 0;
-	for (int i = 0; i < r; i++)
-		for (int j = 0; j < c; j++)
-			if (dis[i][j][1] >= 2)
-				res++;
-	cout << res << '\n';
+	vector<pair<int, pair<int, int>>> d;
+	for (int i = 0; i < (int)person.size(); i++) {
+		for (int j = 0; j < (int)chair.size(); j++) {
+			int distance = POW(person[i].first, person[i].second, chair[j].first, chair[j].second);
+			d.push_back(make_pair(distance, make_pair(i, j)));
+		}
+	}
+	sort(d.begin(), d.end());
+	int cnt = 0;
+	int dis = 0;
+	bool find = false;
+	int pre_y, pre_x;
+	for (pair<int, pair<int, int>> p : d) {
+		if (visited[p.second.first]) continue;
+		int y = chair[p.second.second].first;
+		int x = chair[p.second.second].second;
+		if (map[y][x]) continue;
+		if (dis != p.first) {
+			if (dis) map[pre_y][pre_x] = true;
+			
+			dis = p.first;
+			find = false;
+			pre_y = y;
+			pre_x = x;
+		}
+		else {
+			if (!find) cnt++;
+			find = true;
+		}
+		visited[p.second.first] = true;
+	}
+	cout << cnt << '\n';
 	return 0;
 }
