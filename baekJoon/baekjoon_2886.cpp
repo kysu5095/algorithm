66260a7a571ca1f0ec6a7ab2d9a88f80c204b45e
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-02-07 16:57:51 +0900

    [Gold 1] 자리 전쟁

--- baekJoon/baekjoon_2886.cpp
+++ baekJoon/baekjoon_2886.cpp
@@ -4,15 +4,18 @@
 #include <algorithm>
 #include <vector>
 #include <math.h>
+#include <map>
+#include <set>
 #define POW(A, B, C, D) pow(A-C, 2) + pow(B-D, 2)
 
 using namespace std;
 
 int r, c;
 char arr[101][101];
-bool map[101][101] = { {false,}, };
+int cnt_arr[101][101][2] = { { {false, }, }, };
 bool visited[10001] = { false, };
 vector<pair<int, int>> chair, person;
+map<int, int> m;
 
 int main() {
 	cin.sync_with_stdio(0);
@@ -30,32 +33,42 @@ int main() {
 		for (int j = 0; j < (int)chair.size(); j++) {
 			int distance = POW(person[i].first, person[i].second, chair[j].first, chair[j].second);
 			d.push_back(make_pair(distance, make_pair(i, j)));
+			m[distance]++;
 		}
 	}
 	sort(d.begin(), d.end());
-	int cnt = 0;
-	int dis = 0;
-	bool find = false;
-	int pre_y, pre_x;
-	for (pair<int, pair<int, int>> p : d) {
-		if (visited[p.second.first]) continue;
-		int y = chair[p.second.second].first;
-		int x = chair[p.second.second].second;
-		if (map[y][x]) continue;
-		if (dis != p.first) {
-			if (dis) map[pre_y][pre_x] = true;
-			
-			dis = p.first;
-			find = false;
-			pre_y = y;
-			pre_x = x;
+
+	int res = 0;
+	int idx = 0;
+	set<pair<int, int>> s;
+	set<pair<int, int>>::iterator s_iter;
+	map<int, int>::iterator m_iter;
+	for (m_iter = m.begin(); m_iter != m.end(); m_iter++) {
+		s.clear();
+		int len = m_iter->second;
+		while (len--) {
+			int num = d[idx].second.first;
+			if (visited[num]) {
+				idx++;
+				continue;
+			}
+			int y = chair[d[idx].second.second].first;
+			int x = chair[d[idx].second.second].second;
+			s.insert(make_pair(y, x));
+			if (cnt_arr[y][x][0]) {
+				idx++;
+				continue;
+			}
+			visited[num] = true;
+			if (cnt_arr[y][x][1]++ == 1) res++;
+			idx++;
 		}
-		else {
-			if (!find) cnt++;
-			find = true;
+		for (s_iter = s.begin(); s_iter != s.end(); s_iter++) {
+			int y = s_iter->first;
+			int x = s_iter->second;
+			cnt_arr[y][x][0]++;
 		}
-		visited[p.second.first] = true;
 	}
-	cout << cnt << '\n';
+	cout << res << '\n';
 	return 0;
 }
