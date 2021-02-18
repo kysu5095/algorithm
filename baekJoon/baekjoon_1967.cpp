2c3871ddf6feffb82c07a213cffbd72eb13b6d66
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-02-18 15:34:59 +0900

    [Gold 4] 트리의 지름

--- baekJoon/baekjoon_1967.cpp
+++ baekJoon/baekjoon_1967.cpp
@@ -1,43 +1,42 @@
 // 트리의 지름
 
 #include <iostream>
-#include <vector>
 #include <string.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int n, first_node, res;
-bool visited[10001] = { false, };
+int n, far, res = 0;
+bool visited[10001];
 vector<pair<int, int>> v[10001];
 
 void dfs(int node, int cost) {
-	if (cost > res) {
+	if (visited[node]) return;
+	visited[node] = true;
+	if (res < cost) {
 		res = cost;
-		first_node = node;
+		far = node;
 	}
-	int len = v[node].size();
-	for (int i = 0; i < len; i++) {
-		if (!visited[v[node][i].first]) {
-			visited[v[node][i].first] = true;
-			dfs(v[node][i].first, cost + v[node][i].second);
-		}
-	}
+	for (auto p : v[node])
+		dfs(p.first, cost + p.second);
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int node1, node2, cost;
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	int a, b, c;
 	cin >> n;
-	for (int i = 1; i < n; i++) {
-		cin >> node1 >> node2 >> cost;
-		v[node1].push_back(make_pair(node2, cost));
-		v[node2].push_back(make_pair(node1, cost));
+	for (int i = 0; i < n - 1; i++) {
+		cin >> a >> b >> c;
+		v[a].push_back(make_pair(b, c));
+		v[b].push_back(make_pair(a, c));
 	}
+	memset(visited, false, sizeof(visited));
 	dfs(1, 0);
 	res = 0;
 	memset(visited, false, sizeof(visited));
-	dfs(first_node, 0);
+	dfs(far, 0);
 	cout << res << '\n';
 	return 0;
 }
