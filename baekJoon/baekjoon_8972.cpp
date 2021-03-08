2754198898aa6801a4572ed69a64d0de10b20e0d
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-09 02:05:26 +0900

    [Gold 4] 미친 아두이노

--- baekJoon/baekjoon_8972.cpp
+++ baekJoon/baekjoon_8972.cpp
@@ -2,96 +2,83 @@
 
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <string>
 #include <string.h>
+#include <math.h>
+#include <limits.h>
 
 using namespace std;
 
-int r, c, y, x;
+int r, c;
 char arr[101][101];
-bool visited[101][101][2] = { { {false, }, }, };
-char command[101];
+int visited[101][101];
+string command;
+
 int dy[10] = { 0, 1, 1, 1, 0, 0, 0, -1, -1, -1 };
 int dx[10] = { 0, -1, 0, 1, -1, 0, 1, -1, 0, 1 };
+
 vector<pair<bool, pair<int, int>>> v;
-
-int game() {
-	int ny, nx, d, len, ry, rx, nry, nrx, nnry, nnrx, dist, mdist;
-	bool state;
-	int sz = (int)strlen(command);
-	for (int i = 0; i < sz; i++) {
-		d = command[i] - '0';
-		ny = y + dy[d];
-		nx = x + dx[d];
-		if (arr[ny][nx] == 'R') return i + 1;
-		arr[y][x] = '.';
-		arr[ny][nx] = 'I';
-		y = ny;
-		x = nx;
-
-		len = v.size();
-		for (int idx = 0; idx < len; idx++) {
-			ry = v[idx].second.first;
-			rx = v[idx].second.second;
-			state = v[idx].first;
-			if (!state) continue;
-			arr[ry][rx] = '.';
-
-			mdist = 1000;
-			for (int j = 1; j < 10; j++) {
-				nry = ry + dy[j];
-				nrx = rx + dx[j];
-				dist = abs(nry - y) + abs(nrx - x);
-				if (dist < mdist) {
-					mdist = dist;
-					nnry = nry;
-					nnrx = nrx;
-				}
-			}
-			if (arr[nnry][nnrx] == 'I') return i + 1;
-			if (visited[nnry][nnrx][0]) {
-				visited[nnry][nnrx][1] = true;
-				v[idx].first = false;
-				v[idx].second.first = nnry;
-				v[idx].second.second = nnrx;
-				continue;
-			}
-			visited[nnry][nnrx][0] = true;
-			v[idx].second.first = nnry;
-			v[idx].second.second = nnrx;
-		}
-		for (int idx = 0; idx < len; idx++) {
-			if (!v[idx].first) continue;
-			ry = v[idx].second.first;
-			rx = v[idx].second.second;
-			if (visited[ry][rx][1]) v[idx].first = false;
-			else arr[ry][rx] = 'R';
-		}
-		memset(visited, false, sizeof(visited));
-	}
-	return -1;
-}
 
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
+	int y, x;
 	cin >> r >> c;
 	for (int i = 0; i < r; i++) {
 		cin >> arr[i];
 		for (int j = 0; j < c; j++) {
-			if (arr[i][j] == 'I') y = i, x = j;
+			if (arr[i][j] == 'I') {
+				y = i;
+				x = j;
+			}
 			else if (arr[i][j] == 'R') v.push_back(make_pair(true, make_pair(i, j)));
 		}
 	}
 	cin >> command;
-	int res = game();
-	if (res != -1) cout << "kraj " << res << '\n';
-	else {
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++)
-				cout << arr[i][j];
-			cout << '\n';
+	for (int X = 0; X < command.size(); X++) {
+		arr[y][x] = '.';
+		y += dy[command[X] - '0'];
+		x += dx[command[X] - '0'];
+		if (arr[y][x] == 'R') {
+			cout << "kraj " << X + 1 << '\n';
+			return 0;
 		}
+		arr[y][x] = 'I';
+
+		for (auto& node : v) {
+			if (!node.first) continue;
+			int ny = node.second.first;
+			int nx = node.second.second;
+			arr[ny][nx] = '.';
+			int min_val = INT_MAX;
+			int d;
+			for (int i = 1; i <= 9; i++) {
+				int nny = ny + dy[i];
+				int nnx = nx + dx[i];
+				if (abs(y - nny) + abs(x - nnx) < min_val) {
+					min_val = abs(y - nny) + abs(x - nnx);
+					d = i;
+				}
+			}
+			ny += dy[d];
+			nx += dx[d];
+			if (arr[ny][nx] == 'I') {
+				cout << "kraj " << X + 1 << '\n';
+				return 0;
+			}
+			node.second.first = ny;
+			node.second.second = nx;
+			visited[ny][nx]++;
+		}
+
+		for (auto& node : v) {
+			if (!node.first) continue;
+			if (visited[node.second.first][node.second.second] >= 2) node.first = false;
+			else arr[node.second.first][node.second.second] = 'R';
+		}
+		memset(visited, 0, sizeof(visited));
 	}
+	for (int i = 0; i < r; i++)
+		cout << arr[i] << '\n';
 	return 0;
 }
