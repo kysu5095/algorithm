97c074320f2e61fa1c6609147814f2dfe73accde
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-09 02:42:39 +0900

    [Gold 4] 캐슬 디펜스

--- baekJoon/baekjoon_17135.cpp
+++ baekJoon/baekjoon_17135.cpp
@@ -1,210 +1,88 @@
 // 캐슬 디펜스
-// 11:45
 
 #include <iostream>
-#include <string.h>
-#include <queue>
 #include <math.h>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+#define distance(y1,x1,y2,x2) abs(y1-y2)+abs(x1-x2)
 
 using namespace std;
 
-int n, m, d, enermy, tmp_res, res;
-int arr[16][16];
-int tmp_arr[16][16];
-int kill_map[16][16] = { {0, }, };
-bool visited[16][16];
-int bow[3];
+int n, m, D, shot = 0;
+int position[3];
+vector<pair<bool, pair<int, int>>> origin;
 
-class Point {
+class ARCHER {
 public:
-	int y, x;
+	int idx, d = INT_MAX, x = INT_MAX;
 };
-Point p[3] = { {0, -1}, {-1, 0}, {0, 1} };
 
-void copy_arr() {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			arr[i][j] = tmp_arr[i][j];
-}
-
-void bfs(int _y, int _x, int idx) {
-	memset(visited, false, sizeof(visited));
-	queue<Point> q;
-	q.push({ _y, _x });
-	visited[_y][_x] = true;
-	int y, x, ny, nx, nd;
-	while (!q.empty()) {
-		y = q.front().y;
-		x = q.front().x;
-		q.pop();
-		nd = abs(n - y) + abs(bow[idx] - x);
-		if (nd == d) return;
-
-		for (int i = 0; i < 3; i++) {
-			ny = y + p[i].y;
-			nx = x + p[i].x;
-			if (ny >= 0 && nx >= 0 && nx < m && !visited[ny][nx]) {
-				q.push({ ny, nx });
-				visited[ny][nx] = true;
-				if (arr[ny][nx]) {
-					kill_map[ny][nx] = 1;
-					return;
+int game() {
+	int kill = 0;
+	int len = origin.size();
+	vector<pair<bool, pair<int, int>>> v = origin;
+	while (len) {
+		ARCHER archer[3];
+		for (int i = 0; i < v.size(); i++) {
+			if (!v[i].first) continue;
+			int y = v[i].second.first;
+			int x = v[i].second.second;
+			for (int j = 0; j < 3; j++) {
+				int d = distance(y, x, n, position[j]);
+				if (d <= D) {
+					if (d < archer[j].d || (d == archer[j].d && x < archer[j].x)) {
+						archer[j].idx = i;
+						archer[j].d = d;
+						archer[j].x = x;
+					}
 				}
 			}
 		}
-	}
-}
 
-int kill() {
-	int k = 0;
-	for (int i = 0; i < 3; i++) {
-		if (arr[n - 1][bow[i]]) kill_map[n - 1][bow[i]] = 1;
-		else bfs(n - 1, bow[i], i);
-	}
+		for (int i = 0; i < 3; i++) {
+			if (archer[i].d == INT_MAX) continue;
+			if (!v[archer[i].idx].first) continue;
+			v[archer[i].idx].first = false;
+			len--;
+			kill++;
+		}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (kill_map[i][j]) {
-				k++;
-				tmp_res++;
-				kill_map[i][j] = 0;
-				arr[i][j] = 0;
+		for (int i = 0; i < v.size(); i++) {
+			if (!v[i].first) continue;
+			v[i].second.first++;
+			if (v[i].second.first == n) {
+				v[i].first = false;
+				len--;
 			}
 		}
 	}
-	return k;
-}
-
-int move() {
-	int k = 0;
-	for (int i = n - 1; i >= 0; i--) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j]) {
-				arr[i][j] = 0;
-				if (i == n - 1) k++;
-				else arr[i + 1][j] = 1;
-			}
-		}
-	}
-	return k;
+	return kill;
 }
 
 void dfs(int idx, int cnt) {
 	if (cnt == 3) {
-		tmp_res = 0;
-		int tmp = enermy;
-		while (tmp) {
-			tmp -= kill();
-			tmp -= move();
-		}
-		copy_arr();
-		res = res < tmp_res ? tmp_res : res;
+		shot = max(shot, game());
 		return;
 	}
 	for (int i = idx; i < m; i++) {
-		bow[cnt] = i;
+		position[cnt] = i;
 		dfs(i + 1, cnt + 1);
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> n >> m >> d;
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	int val;
+	cin >> n >> m >> D;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-			tmp_arr[i][j] = arr[i][j];
-			if (arr[i][j])
-				enermy++;
+			cin >> val;
+			if (val) origin.push_back(make_pair(true, make_pair(i, j)));
 		}
 	}
 	dfs(0, 0);
-	cout << res << '\n';
+	cout << shot << '\n';
 	return 0;
 }
-
-// #include <iostream>
-// #include <algorithm>
-// #include <vector>
-// #include <math.h>
-// #include <set>
-
-// using namespace std;
-
-// int n, m, d, res = 0;
-// bool arr[16][16];
-// int arch[3];
-// vector<pair<bool, pair<int, int>>> enermy;
-
-// int game() {
-// 	vector<pair<bool, pair<int, int>>> v = enermy;
-// 	int y, x, ay = n, ax, distance, cnt = 0;
-// 	int len = v.size();
-// 	int res_enermy = len;
-// 	while (res_enermy) {
-// 		set<int> hunt;
-// 		for (int idx = 0; idx < 3; idx++) {
-// 			ax = arch[idx];
-// 			vector<pair<int, pair<int, int>>> tmp;
-// 			for (int i = 0; i < len; i++) {
-// 				if (v[i].first == false) continue;
-// 				y = v[i].second.first;
-// 				x = v[i].second.second;
-// 				distance = abs(ay - y) + abs(ax - x);
-// 				if (distance <= d) tmp.push_back(make_pair(distance, make_pair(x, i)));
-// 			}
-// 			if (!tmp.empty()) {
-// 				sort(tmp.begin(), tmp.end());
-// 				hunt.insert(tmp[0].second.second);
-// 			}
-// 		}
-		
-// 		if (!hunt.empty()) {
-// 			set<int>::iterator iter;
-// 			for (iter = hunt.begin(); iter != hunt.end(); iter++) {
-// 				v[*iter].first = false;
-// 				cnt++;
-// 				res_enermy--;
-// 			}
-// 		}
-
-// 		for (int i = 0; i < len; i++) {
-// 			if (v[i].first == false) continue;
-// 			v[i].second.first++;
-// 			if (v[i].second.first == n) {
-// 				v[i].first = false;
-// 				res_enermy--;
-// 			}
-// 		}
-// 	}
-// 	return cnt;
-// }
-
-// void dfs(int idx, int cnt) {
-// 	if (cnt == 3) {
-// 		int val = game();
-// 		res = max(res, val);
-// 		return;
-// 	}
-// 	for (int i = idx; i < m; i++) {
-// 		arch[cnt] = i;
-// 		dfs(i + 1, cnt + 1);
-// 	}
-// }
-
-// int main() {
-// 	cin.sync_with_stdio(0);
-// 	cin.tie(0);
-// 	cin >> n >> m >> d;
-// 	for (int i = 0; i < n; i++) {
-// 		for (int j = 0; j < m; j++) {
-// 			cin >> arr[i][j];
-// 			if (arr[i][j]) 
-// 				enermy.push_back(make_pair(true, make_pair(i, j)));
-// 		}
-// 	}
-// 	dfs(0, 0);
-// 	cout << res << '\n';
-// 	return 0;
-// }
