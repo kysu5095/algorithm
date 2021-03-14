1aeb626bb2b0db401df68d05a8249d9de7dcf2ee
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-15 01:52:41 +0900

    [Gold 5] 골목 대장 호석 - 기능성

--- baekJoon/baekjoon_20168.py
+++ baekJoon/baekjoon_20168.py
@@ -4,31 +4,28 @@ import sys
 from queue import PriorityQueue
 input = sys.stdin.readline
 
-def bfs():
-    pq = PriorityQueue()
-    pq.put([0, A, 0])
-    visited[A] = 0
-    while not pq.empty():
-        cost, node, max_val = pq.get()
-        if node == B:
-            res.append(max_val)
-            continue
-        if cost > visited[node]: continue
-        for next, next_cost in v[node]:
-            if cost + next_cost > C: continue
-            if cost + next_cost >= visited[next]: continue
-            pq.put([cost + next_cost, next, max(max_val, next_cost)])
-            visited[next] = cost + next_cost
-
 n, m, A, B, C = map(int, input().split())
 v = [[] for _ in range(n + 1)]
-visited = [float('inf') for _ in range(n + 1)]
+visited = [False] * (n + 1)
 res = list()
 for _ in range(m):
     node1, node2, distance = map(int, input().split())
     v[node1].append([node2, distance])
     v[node2].append([node1, distance])
 
-bfs()
+def dfs(node, cost, max_val):
+    if node == B:
+        res.append(max_val)
+        return
+    for next, next_cost in v[node]:
+        if visited[next]: continue
+        if cost + next_cost > C: continue
+        visited[next] = True
+        dfs(next, cost + next_cost, max(max_val, next_cost))
+        visited[next] = False
+
+visited[A] = True
+dfs(A, 0, 0)
+
 if len(res) == 0: print(-1)
 else: print(min(res))
