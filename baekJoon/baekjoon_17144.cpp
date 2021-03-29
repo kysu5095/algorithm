e42045139b5dc9ec3a92b62e4a356a00702c5191
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-29 17:09:23 +0900

    [Gold 5] 미세먼지 안녕!

--- baekJoon/baekjoon_17144.cpp
+++ baekJoon/baekjoon_17144.cpp
@@ -1,261 +1,87 @@
-// 미세먼지 안녕! //
-///////////////////
+// 미세먼지 안녕!
 
-#include <iostream>
-#include <vector>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <queue>
+#include <string.h>
 
-using namespace std;
+int r, c, t, res = 0;
+int arr[51][51], tmp[51][51];
+int dy[2][4] = { {0, -1, 0, 1}, {0, 1, 0, -1} };
+int dx[2][4] = { {1, 0, -1, 0}, {1, 0, -1, 0} };
+std::pair<int, int> p[2];
 
-int sum = 0;
-int r, c, t;
-int arr[52][52];
+void spread() {
+	memset(tmp, 0, sizeof(tmp));
+	tmp[p[0].first][p[0].second] = -1;
+	tmp[p[1].first][p[1].second] = -1;
 
-class Point {
-public:
-	int x, y;
-};
-Point p[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-Point unclockD[4] = { {1, 0}, {0, -1}, {-1, 0}, {0, 1} };	// 반시계 방향
-Point clockD[4] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };		// 시계 방향
-vector<Point> air;
-vector<Point> warm;
-
-void condition() {
-	for (int i = 0; i < 2; i++) {
-		int x = air[i].x + 1;
-		int y = air[i].y;
-		int res[3] = { 0, };
-
-		if (i == 0) {	// 반시계 방향
-			int direct = 0; // 방향
-			res[0] = x; res[1] = y; res[2] = arr[y][x];
-			arr[y][x] = 0;
-			while (1) {
-				int tmpX = res[0];
-				int tmpY = res[1];
-				int tmpV = res[2];
-				int nx = tmpX + unclockD[direct % 4].x;
-				int ny = tmpY + unclockD[direct % 4].y;
-
-				if (nx < 0 || nx >= c || ny < 0) direct++; // 방향 바꿔주기
-				nx = tmpX + unclockD[direct % 4].x;
-				ny = tmpY + unclockD[direct % 4].y;
-				
-				if (arr[ny][nx] == -1) { // 종료 조건
-					break;
-				}
-				res[0] = nx; res[1] = ny; res[2] = arr[ny][nx];
-				arr[ny][nx] = tmpV;
+	for (int y = 0; y < r; y++) {
+		for (int x = 0; x < c; x++) {
+			if (arr[y][x] <= 0) continue;
+			tmp[y][x] += arr[y][x];
+			if (tmp[y][x] < 5) continue;
+			int dir = 0;
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[0][i];
+				int nx = x + dx[0][i];
+				if (ny < 0 || ny == r || nx < 0 || nx == c || tmp[ny][nx] == -1) continue;
+				tmp[ny][nx] += arr[y][x] / 5;
+				dir++;
 			}
-		}
-		else {	// 시계 방향
-			int direct = 0; // 방향
-			res[0] = x; res[1] = y; res[2] = arr[y][x];
-			arr[y][x] = 0;
-			while (1) {
-				int tmpX = res[0];
-				int tmpY = res[1];
-				int tmpV = res[2];
-				int nx = tmpX + clockD[direct % 4].x;
-				int ny = tmpY + clockD[direct % 4].y;
-
-				if (nx < 0 || nx >= c || ny >= r) direct++; // 방향 바꿔주기
-				nx = tmpX + clockD[direct % 4].x;
-				ny = tmpY + clockD[direct % 4].y;
-				
-				if (arr[ny][nx] == -1) { // 종료 조건
-					break;
-				}
-				res[0] = nx; res[1] = ny; res[2] = arr[ny][nx];
-				arr[ny][nx] = tmpV;
-			}
+			tmp[y][x] -= (arr[y][x] / 5) * dir;
 		}
 	}
+	
+	for (int i = 0; i < r; i++)
+		for (int j = 0; j < c; j++)
+			arr[i][j] = tmp[i][j];
 }
 
-void spread(int time) {
-	for (int i = 0; i < time; i++) {			// 몇 초 동안 작동할 것인가
-		int arr2[52][52] = { {0, } };			// 미세먼지 임시 저장 배열
-		arr2[air[0].y][air[0].x] = -1;
-		arr2[air[1].y][air[1].x] = -1;
-		int warmSize = warm.size();
-
-		for (int j = 0; j < warmSize; j++) {	// 미세먼지 퍼트리기
-			int warmCnt = 0;					// 퍼진 방향의 개수
-
-			for (int k = 0; k < 4; k++) {		// 네 방향 확인
-				int nx = warm[j].x + p[k].x;
-				int ny = warm[j].y + p[k].y;
-
-				if (nx >= 0 && nx < c && ny >= 0 && ny < r && arr[ny][nx] != -1) {
-					arr2[ny][nx] += arr[warm[j].y][warm[j].x] / 5;
-					warmCnt++;
+void aircondition() {
+	int y, x, cur, next;
+	for (int idx = 0; idx < 2; idx++) {
+		y = p[idx].first;
+		x = p[idx].second + 1;
+		cur = arr[y][x];
+		arr[y][x] = 0;
+		for (int i = 0; i < 4; i++) {
+			while (true) {
+				y += dy[idx][i];
+				x += dx[idx][i];
+				if (y < 0 || y == r || x < 0 || x == c || arr[y][x] == -1) {
+					y -= dy[idx][i];
+					x -= dx[idx][i];
+					break;
 				}
+				next = arr[y][x];
+				arr[y][x] = cur;
+				cur = next;
 			}
-			arr2[warm[j].y][warm[j].x] += arr[warm[j].y][warm[j].x] - ((arr[warm[j].y][warm[j].x] / 5) * warmCnt);
-			///////////////// j번째 미세먼지 퍼트리기 완료
-		}
-		//cout << '\n';
-		//cout << "공기청정기 작동전\n";
-		warm.clear(); // 미세먼지 정보 초기화
-		for (int j = 0; j < r; j++) {	// 퍼진 미세먼지 뿌리기
-			for (int k = 0; k < c; k++) {
-				arr[j][k] = arr2[j][k];
-				//cout << arr[j][k] << " ";
-			}
-			//cout << '\n';
-		}
-		//cout << '\n';
-		//cout << "공기청정기 작동후\n"; 
-		condition(); // 공기청정기 작동
-		for (int j = 0; j < r; j++) {	// 한칸 이동된 미세먼지 정보 저장
-			for (int k = 0; k < c; k++) {
-				if (arr[j][k] != 0 && arr[j][k] != -1) warm.push_back({ k, j });
-				//cout << arr[j][k] << " ";
-			}
-			//cout << '\n';
 		}
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> r >> c >> t;
+	int idx = 0;
+	scanf("%d %d %d", &r, &c, &t);
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			cin >> arr[i][j];
-			if (arr[i][j] == -1) air.push_back({ j, i });
-			if (arr[i][j] != 0 && arr[i][j] != -1) warm.push_back({ j, i });
+			scanf("%d", &arr[i][j]);
+			if (arr[i][j] == -1) {
+				p[idx].first = i;
+				p[idx++].second = j;
+			}
 		}
 	}
-	spread(t);
-
-	for (int i = 0; i < warm.size(); i++) {
-		sum += arr[warm[i].y][warm[i].x];
+	while (t--) {
+		spread();
+		aircondition();
 	}
-	cout << sum << '\n';
-
+	for (int i = 0; i < r; i++)
+		for (int j = 0; j < c; j++)
+			if (arr[i][j] > 0)
+				res += arr[i][j];
+	printf("%d\n", res);
 	return 0;
 }
-
-// #include <iostream>
-// #include <queue>
-
-// using namespace std;
-
-// int r, c, t;
-// int arr[51][51];
-// int dy[4] = { 0, 0, 1, -1 };
-// int dx[4] = { 1, -1, 0, 0 };
-// int clock_dir[4] = { 0, 2, 1, 3 };
-// int uclock_dir[4] = { 0, 3, 1, 2 };
-// pair<int, int> air[2];
-
-// void aircondition(int tmp[][51]) {
-// 	for (int i = 0; i < r; i++)
-// 		for (int j = 0; j < c; j++)
-// 			arr[i][j] = tmp[i][j];
-// 	int y = air[0].first;
-// 	int x = air[0].second;
-// 	int d = 0;
-// 	y += dy[uclock_dir[d]];
-// 	x += dx[uclock_dir[d]];
-// 	arr[y][x] = 0;
-// 	int ny, nx;
-// 	while (true) {
-// 		ny = y + dy[uclock_dir[d]];
-// 		nx = x + dx[uclock_dir[d]];
-// 		if (ny < 0 || ny == r || nx < 0 || nx == c) {
-// 			d++;
-// 			ny = y + dy[uclock_dir[d]];
-// 			nx = x + dx[uclock_dir[d]];
-// 		}
-// 		if (ny == air[0].first && nx == air[0].second) break;
-// 		arr[ny][nx] = tmp[y][x];
-// 		y = ny;
-// 		x = nx;
-// 	}
-
-// 	y = air[1].first;
-// 	x = air[1].second;
-// 	d = 0;
-// 	y += dy[clock_dir[d]];
-// 	x += dx[clock_dir[d]];
-// 	arr[y][x] = 0;
-// 	while (true) {
-// 		ny = y + dy[clock_dir[d]];
-// 		nx = x + dx[clock_dir[d]];
-// 		if (ny < 0 || ny == r || nx < 0 || nx == c) {
-// 			d++;
-// 			ny = y + dy[clock_dir[d]];
-// 			nx = x + dx[clock_dir[d]];
-// 		}
-// 		if (ny == air[1].first && nx == air[1].second) break;
-// 		arr[ny][nx] = tmp[y][x];
-// 		y = ny;
-// 		x = nx;
-// 	}
-// }
-
-// void spread() {
-// 	queue<pair<int, int>> q;
-// 	for (int i = 0; i < r; i++)
-// 		for (int j = 0; j < c; j++)
-// 			if (arr[i][j] > 0)
-// 				q.push(make_pair(i, j));
-// 	int tmp[51][51] = { {0,}, };
-// 	tmp[air[0].first][air[0].second] = -1;
-// 	tmp[air[1].first][air[1].second] = -1;
-
-// 	int y, x, ny, nx, val, cnt;
-// 	while (!q.empty()) {
-// 		y = q.front().first;
-// 		x = q.front().second;
-// 		q.pop();
-// 		cnt = 0;
-// 		for (int i = 0; i < 4; i++) {
-// 			ny = y + dy[i];
-// 			nx = x + dx[i];
-// 			if (ny < 0 || ny == r || nx < 0 || nx == c || tmp[ny][nx] == -1) continue;
-// 			cnt++;
-// 		}
-// 		if (!cnt) continue;
-// 		val = arr[y][x] / 5;
-// 		tmp[y][x] = tmp[y][x] + (arr[y][x] - (val * cnt));
-// 		if (!val) continue;
-// 		for (int i = 0; i < 4; i++) {
-// 			ny = y + dy[i];
-// 			nx = x + dx[i];
-// 			if (ny < 0 || ny == r || nx < 0 || nx == c || tmp[ny][nx] == -1) continue;
-// 			tmp[ny][nx] += val;
-// 		}
-// 	}
-// 	aircondition(tmp);
-// }
-
-// int main() {
-// 	cin.sync_with_stdio(0);
-// 	cin.tie(0);
-// 	int idx = 0;
-// 	cin >> r >> c >> t;
-// 	for (int i = 0; i < r; i++) {
-// 		for (int j = 0; j < c; j++) {
-// 			cin >> arr[i][j];
-// 			if (arr[i][j] == -1) {
-// 				air[idx].first = i;
-// 				air[idx++].second = j;
-// 			}
-// 		}
-// 	}
-// 	while (t--) 
-// 		spread();
-// 	int res = 0;
-// 	for (int i = 0; i < r; i++)
-// 		for (int j = 0; j < c; j++)
-// 			if (arr[i][j] > 0)
-// 				res += arr[i][j];
-// 	cout << res << '\n';
-// 	return 0;
-// }
