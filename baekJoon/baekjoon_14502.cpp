f3ff76ae182b58e24609b07a83daf9d56f73dc67
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-29 17:30:27 +0900

    [Gold 5] 연구소

--- baekJoon/baekjoon_14502.cpp
+++ baekJoon/baekjoon_14502.cpp
@@ -1,104 +1,169 @@
-// 연구소 //
-///////////
+// 연구소
 
-//////////////////////////////////////////////////////////////////////
-//		1번 코드 : 처음 푼 코드(36ms)
-//		2번 코드 : 복습 후 코드(24ms) >> stay전역 변수로 계산하는 시간 줄임
-//////////////////////////////////////////////////////////////////////
-
-// 0 -> 빈칸(3~), 1 -> 벽, 2 -> 바이러스(2~10)
-#include <iostream>
-#include <cstring>
-#include <vector>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
 #include <algorithm>
+#include <vector>
+#include <queue>
 
-using namespace std;
+int n, m, cnt = 0, res = 0;
+int arr[9][9];
+bool visited[9][9];
+int dy[4] = { 0, 0, 1, -1 };
+int dx[4] = { 1, -1, 0, 0 };
+std::vector<std::pair<int, int>> v, wall;
 
-int n, m;
-int maxCnt = 0;
-int arr[8][8] = { {-1, } };     // 기존배열
-int arr2[8][8] = { {-1, } };    // 벽을 세운 새로운 배열
-bool visited[8][8] = { {false, } }; // 벽 세우기
-
-class Point {
-public:
-	int x, y;
-};
-Point p[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-vector<Point> v; // 지나갈 수 있는 통로 저장 벡터 ('0')
-vector<Point> q;  // 바이러스 저장 큐
-
-void dfs(int x, int y); // 바이러스 퍼트리기
-void makeWall(int idx, int cnt); // 벽을 만들자
-void copyArr(void); // 기존 배열과 동일한 배열 >> 이 copy배열에 벽을 세울거임
+void bfs() {
+	memset(visited, false, sizeof(visited));
+	std::queue<std::pair<int, int>> q;
+	for (int i = 0; i < v.size(); i++) {
+		q.push(std::make_pair(v[i].first, v[i].second));
+		visited[v[i].first][v[i].second] = true;
+	}
+	int cnt = wall.size() - 3;
+	int y, x, ny, nx;
+	while (!q.empty()) {
+		y = q.front().first;
+		x = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			ny = y + dy[i];
+			nx = x + dx[i];
+			if (ny < 0 || ny == n || nx < 0 || nx == m || visited[ny][nx] || arr[ny][nx]) continue;
+			q.push(std::make_pair(ny, nx));
+			visited[ny][nx] = true;
+			cnt--;
+		}
+		if (res >= cnt) return;
+	}
+	res = std::max(res, cnt);
+}
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> n >> m;
+	scanf("%d %d", &n, &m);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-			if (arr[i][j] == 0) v.push_back({ j, i });
-			if (arr[i][j] == 2) q.push_back({ j, i });
+			scanf("%d", &arr[i][j]);
+			if (arr[i][j] == 2) v.push_back(std::make_pair(i, j));
+			else if (!arr[i][j]) wall.push_back(std::make_pair(i, j));
 		}
 	}
-	makeWall(0, 0);
-	cout << maxCnt << '\n';
+	std::vector<int> tmp(wall.size(), 0);
+	tmp[wall.size() - 3] = 1;
+	tmp[wall.size() - 2] = 1;
+	tmp[wall.size() - 1] = 1;
+	do {
+		for (int i = 0; i < wall.size(); i++) if (tmp[i]) arr[wall[i].first][wall[i].second] = 1;
+		bfs();
+		for (int i = 0; i < wall.size(); i++) if (tmp[i]) arr[wall[i].first][wall[i].second] = 0;
+	} while (std::next_permutation(tmp.begin(), tmp.end()));
 
+	printf("%d\n", res);
 	return 0;
 }
 
-void copyArr(void) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			arr2[i][j] = arr[i][j];
-		}
-	}
-}
+// // 연구소 //
+// ///////////
 
-void makeWall(int idx, int cnt) {
-	if (cnt == 3) {
-		int Acnt = 0;
-		copyArr();
-		for (int i = 0; i < q.size(); i++) { // 병균 퍼트리기
-			dfs(q[i].x, q[i].y);
-		}
-		for (int i = 0; i < n; i++) { // 안전한 곳 찾기
-			for (int j = 0; j < m; j++) {
-				if (arr2[i][j] == 0) Acnt++;
-			}
-		}
+// //////////////////////////////////////////////////////////////////////
+// //		1번 코드 : 처음 푼 코드(36ms)
+// //		2번 코드 : 복습 후 코드(24ms) >> stay전역 변수로 계산하는 시간 줄임
+// //////////////////////////////////////////////////////////////////////
 
-		maxCnt = max(maxCnt, Acnt);
+// // 0 -> 빈칸(3~), 1 -> 벽, 2 -> 바이러스(2~10)
+// #include <iostream>
+// #include <cstring>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int n, m;
+// int maxCnt = 0;
+// int arr[8][8] = { {-1, } };     // 기존배열
+// int arr2[8][8] = { {-1, } };    // 벽을 세운 새로운 배열
+// bool visited[8][8] = { {false, } }; // 벽 세우기
+
+// class Point {
+// public:
+// 	int x, y;
+// };
+// Point p[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+// vector<Point> v; // 지나갈 수 있는 통로 저장 벡터 ('0')
+// vector<Point> q;  // 바이러스 저장 큐
+
+// void dfs(int x, int y); // 바이러스 퍼트리기
+// void makeWall(int idx, int cnt); // 벽을 만들자
+// void copyArr(void); // 기존 배열과 동일한 배열 >> 이 copy배열에 벽을 세울거임
+
+// int main() {
+// 	ios_base::sync_with_stdio(false);
+// 	cin.tie(0);
+
+// 	cin >> n >> m;
+// 	for (int i = 0; i < n; i++) {
+// 		for (int j = 0; j < m; j++) {
+// 			cin >> arr[i][j];
+// 			if (arr[i][j] == 0) v.push_back({ j, i });
+// 			if (arr[i][j] == 2) q.push_back({ j, i });
+// 		}
+// 	}
+// 	makeWall(0, 0);
+// 	cout << maxCnt << '\n';
+
+// 	return 0;
+// }
+
+// void copyArr(void) {
+// 	for (int i = 0; i < n; i++) {
+// 		for (int j = 0; j < m; j++) {
+// 			arr2[i][j] = arr[i][j];
+// 		}
+// 	}
+// }
+
+// void makeWall(int idx, int cnt) {
+// 	if (cnt == 3) {
+// 		int Acnt = 0;
+// 		copyArr();
+// 		for (int i = 0; i < q.size(); i++) { // 병균 퍼트리기
+// 			dfs(q[i].x, q[i].y);
+// 		}
+// 		for (int i = 0; i < n; i++) { // 안전한 곳 찾기
+// 			for (int j = 0; j < m; j++) {
+// 				if (arr2[i][j] == 0) Acnt++;
+// 			}
+// 		}
+
+// 		maxCnt = max(maxCnt, Acnt);
 		
-		return;
-	}
+// 		return;
+// 	}
 
-	for (int i = idx; i < v.size(); i++) {
-		int nextX = v[i].x;
-		int nextY = v[i].y;
+// 	for (int i = idx; i < v.size(); i++) {
+// 		int nextX = v[i].x;
+// 		int nextY = v[i].y;
 
-		arr[nextY][nextX] = 1;
-		makeWall(i + 1, cnt + 1);
-		arr[nextY][nextX] = 0;
-	}
-}
+// 		arr[nextY][nextX] = 1;
+// 		makeWall(i + 1, cnt + 1);
+// 		arr[nextY][nextX] = 0;
+// 	}
+// }
 
-void dfs(int x, int y) {
-	for (int i = 0; i < 4; i++) {
-		int nextX = x + p[i].x;
-		int nextY = y + p[i].y;
+// void dfs(int x, int y) {
+// 	for (int i = 0; i < 4; i++) {
+// 		int nextX = x + p[i].x;
+// 		int nextY = y + p[i].y;
 
-		if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n) {
-			if (arr2[nextY][nextX] == 0) {
-				arr2[nextY][nextX] = 2;
-				dfs(nextX, nextY);
-			}
-		}
-	}
-}
+// 		if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n) {
+// 			if (arr2[nextY][nextX] == 0) {
+// 				arr2[nextY][nextX] = 2;
+// 				dfs(nextX, nextY);
+// 			}
+// 		}
+// 	}
+// }
 
 // // 복습_연구소 //
 // ////////////////
