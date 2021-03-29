df18cf74541067791b9049832c3be04fb70d18f9
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-29 12:05:42 +0900

    [Gold 5] 게리맨더링

--- baekJoon/baekjoon_17471.cpp
+++ baekJoon/baekjoon_17471.cpp
@@ -1,92 +1,100 @@
-// 게리맨더링 //
+// 게리맨더링
 
-#include <iostream>
-#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <vector>
 #include <queue>
+#include <limits.h>
+#include <string.h>
+#include <algorithm>
 #include <math.h>
 
 using namespace std;
 
-int n, min_gap = 10000;
-int arr[11];
-bool is_team[11];
+int n, res = INT_MAX;
+int arr[11], people[11];
+bool visited[11], team_a[11], team_b[11];
 vector<int> v[11];
 
-bool bfs(vector<int> team) {
-	if (team.size() == 1) return true;
-	int len = team.size(), val;
-	bool visited[11] = { false };
-	visited[team[0]] = true;
+bool find() {
 	queue<int> q;
-	q.push(team[0]);
+	memset(visited, false, sizeof(visited));
+	for (int i = 0; i < n; i++) {
+		if (team_a[i]) {
+			q.push(i);
+			visited[i] = true;
+			break;
+		}
+	}
 	while (!q.empty()) {
-		val = q.front();
-		for (int i = 0; i < v[val].size(); i++) {
-			for (int j = 0; j < len; j++) {
-				if (v[val][i] == team[j] && !visited[team[j]]) {
-					visited[team[j]] = true;
-					q.push(team[j]);
-					break;
-				}
+		int node = q.front();
+		q.pop();
+		for (int& next : v[node]) {
+			if (!visited[next] && team_a[next]) {
+				q.push(next);
+				visited[next] = true;
 			}
 		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (team_b[i]) {
+			q.push(i);
+			visited[i] = true;
+			break;
+		}
+	}
+	while (!q.empty()) {
+		int node = q.front();
 		q.pop();
+		for (int& next : v[node]) {
+			if (!visited[next] && team_b[next]) {
+				q.push(next);
+				visited[next] = true;
+			}
+		}
 	}
-	for (int i = 0; i < len; i++) {
-		if (!visited[team[i]])
+	for (int i = 0; i < n; i++)
+		if (!visited[i])
 			return false;
-	}
 	return true;
 }
 
-void dfs(int idx, int cnt, vector<int> team_A, vector<int> team_B) {
-	if (idx == cnt) {
-		for (int i = 0; i < n; i++) {
-			if (is_team[i]) team_A.push_back(i);
-			else team_B.push_back(i);
-		}
-		if (bfs(team_A) && bfs(team_B)) {
-			int min_A = 0, min_B = 0;
-			for (int i = 0; i < team_A.size(); i++) min_A += arr[team_A[i]];
-			for (int i = 0; i < team_B.size(); i++) min_B += arr[team_B[i]];
-			min_A = abs(min_A - min_B);
-			min_gap = min_gap > min_A ? min_A : min_gap;
-		}
-		team_A.clear();
-		team_B.clear();
-		return;
-	}
-	for (int i = 0; i < n; i++) {
-		if (!is_team[i]) {
-			is_team[i] = true;
-			dfs(idx + 1, cnt, team_A, team_B);
-			is_team[i] = false;
-		}
-	}
-}
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int input, input2;
-	vector<int> team_A;
-	vector<int> team_B;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d", &people[i]);
 	for (int i = 0; i < n; i++) {
-		cin >> input;
-		for (int j = 0; j < input; j++) {
-			cin >> input2;
-			v[i].push_back(input2 - 1);
+		int sz;
+		scanf("%d", &sz);
+		for(int j = 0; j < sz; j++) {
+			int num;
+			scanf("%d", &num);
+			v[i].push_back(num - 1);
+			v[num - 1].push_back(i);
 		}
 	}
-	for (int i = 1; i <= n / 2; i++) 
-		dfs(0, i, team_A, team_B);
-	if(min_gap != 10000)
-		cout << min_gap << '\n';
-	else
-		cout << -1 << '\n';
+	for (int i = 0; i < n / 2; i++) {
+		arr[n - i - 1] = 1;
+		do {
+			int A = 0, B = 0;
+			memset(team_a, false, sizeof(team_a));
+			memset(team_b, false, sizeof(team_b));
+			for (int j = 0; j < n; j++) {
+				if (arr[j]) {
+					A += people[j];
+					team_a[j] = true;
+				}
+				else {
+					B += people[j];
+					team_b[j] = true;
+				}
+			}
+			if (abs(A - B) < res) {
+				if (find())
+					res = abs(A - B);
+			}
+		} while (next_permutation(arr, arr + n));
+	}
+	if (res == INT_MAX) res = -1;
+	printf("%d\n", res);
 	return 0;
 }
