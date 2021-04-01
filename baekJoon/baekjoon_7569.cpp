e2d54d2e0c32f20ebf08c8b09951fcdb90b23a09
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-02 02:59:37 +0900

    [Silver 1] 토마토

--- baekJoon/baekjoon_7569.cpp
+++ baekJoon/baekjoon_7569.cpp
@@ -1,87 +1,61 @@
-//  토마토2  //
-//////////////
+// 토마토
 
-#include <iostream>
+#include <stdio.h>
 #include <queue>
 
 using namespace std;
 
-int m, n, h;
-int unTomato = 0;
-int arr[100][100][100] = { { {2, } } };
-bool visited[100][100][100] = { { { false, } } };
-
-class Point{
-    public:
-        int h, x, y;
+int n, m, h, cnt = 0;
+int arr[101][101][101];
+int dz[6] = { 0, 0, 0, 0, 1, -1 };
+int dy[6] = { 0, 0, 1, -1, 0, 0 };
+int dx[6] = { 1, -1, 0, 0 ,0 ,0 };
+struct Point {
+	int z, y, x;
 };
-Point p[6] = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
 queue<Point> q;
 
-bool isFull(){
-    int cnt = 0;
-    for(int k = 0; k < h; k++){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(arr[k][i][j] == 1) cnt++;
-            }
-        }
-    }
-    return cnt == m * n * h - unTomato;
+int bfs() {
+	if (!cnt) return 0;
+	int z, y, x, nz, ny, nx, len, t = 0;
+	while (!q.empty()) {
+		t++;
+		len = q.size();
+		while (len--) {
+			z = q.front().z;
+			y = q.front().y;
+			x = q.front().x;
+			q.pop();
+			for (int i = 0; i < 6; i++) {
+				nz = z + dz[i];
+				ny = y + dy[i];
+				nx = x + dx[i];
+				if (nz < 0 || nz == h || ny < 0 || ny == n || nx < 0 || nx == m) continue;
+				if (arr[nz][ny][nx] != 0) continue;
+				q.push({ nz, ny, nx });
+				arr[nz][ny][nx] = -1;
+				cnt--;
+			}
+			if (!cnt) return t;
+		}
+	}
+	return -1;
 }
 
-int bfs(void){
-    int day = 0;
-
-    while(!q.empty()){
-        int size = q.size();
-        for(int i = 0; i < size; i++){
-            int preX = q.front().x;
-            int preY = q.front().y;
-            int preH = q.front().h;
-            visited[preH][preY][preX] = true;
-
-            for(int j = 0; j < 6; j++){
-                int nextX = preX + p[j].x;
-                int nextY = preY + p[j].y;
-                int nextH = preH + p[j].h;
-
-                if(nextX >= 0 && nextX < m && nextY >= 0 && nextY < n && nextH >= 0 && nextH < h){
-                    if(arr[nextH][nextY][nextX] == 0 && visited[nextH][nextY][nextX] == false){
-                        q.push({nextH, nextX, nextY});
-                        visited[nextH][nextY][nextX] = true;
-                        arr[nextH][nextY][nextX] = 1;
-                    }
-                }
-            }
-            q.pop();
-        }
-        if(q.size() == 0 && isFull()) return day;
-        if(q.size() == 0 && !isFull()) return -1;
-
-        day++;
-    }
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    int input;
-    cin >> m >> n >> h;
-    for(int k = 0; k < h; k++){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cin >> input;
-                arr[k][i][j] = input;
-                if(input == 1) q.push({k, j, i});
-                if(input == -1) unTomato++;
-            }
-        }
-    }
-
-    if(q.size() == m * n * h - unTomato) cout << 0 << '\n';
-    else cout << bfs() << '\n';
-
-    return 0;
+int main() {
+	scanf("%d %d %d", &m, &n, &h);
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
+				scanf(" %d", &arr[i][j][k]);
+				if (arr[i][j][k] == 1) {
+					q.push({ i, j, k });
+					arr[i][j][k] = -1;
+				}
+				else if (arr[i][j][k] == 0) cnt++;
+			}
+		}
+	}
+	printf("%d\n", bfs());
+	return 0;
 }
