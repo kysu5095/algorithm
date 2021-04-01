f716876fdc47bac2ed5b42319e71c0dc2d18a14c
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-02 02:46:59 +0900

    [Gold 4] 이차원 배열과 연산

--- baekJoon/baekjoon_17140.cpp
+++ baekJoon/baekjoon_17140.cpp
@@ -1,149 +1,89 @@
-// 이차원 배열과 연산 //
-// for문 연산횟수 줄이기
-// 회전행렬 다시 구하기(회전하는거 구해서 다시))
-// map사용해보기
-//////////////////////
+// 아치원 배열과 연산
 
-#include <iostream>
-#include <queue>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <set>
+#include <map>
 
 using namespace std;
 
-int row, colum, k;
-int maxR = 3, maxC = 3, sec = 0;
-int arr[101][101] = { {0, }, };
-int tmp[101][101] = { {0, }, };
-int countArr[101] = { 0, };
+int r, c, k, N = 3, M = 3, t = 0;
+int arr[101][101], tmp[101][101];
 
-class Point {
-public:
+struct Point {
 	int num, cnt;
-
 	bool operator < (const Point& p) const {
-		if (this->cnt < p.cnt) return false;
+		if (this->cnt < p.cnt) return true;
 		else if (this->cnt == p.cnt) {
-			if (this->num < p.num) return false;
-			else return true;
+			if (this->num < p.num) return true;
+			else return false;
 		}
-		else return true;
+		else return false;
 	}
 };
-priority_queue<Point> pq;
 
-void clearTmp() {
-	for (int i = 0; i < 101; i++)
-		countArr[i] = 0;
-	while (!pq.empty())
-		pq.pop();
-}
-
-void copyArrA() {
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++) {
-			tmp[i][j] = arr[i][j];
-			arr[i][j] = 0;
+void R_operator() {
+	int max_M = 0;
+	for (int i = 0; i < N; i++) {
+		map<int, int> m;
+		for (int j = 0; j < M; j++) {
+			if (!arr[i][j]) continue;
+			m[arr[i][j]]++;
 		}
+		set<Point> s;
+		for (auto& it : m) s.insert({ it.first, it.second });
+		int j = 0;
+		for (auto& it : s) {
+			tmp[i][j++] = it.num;
+			tmp[i][j++] = it.cnt;
+			if (j == 100) break;
+		}
+		max_M = max(max_M, j);
 	}
-}
-
-void copyArrB() {
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++) {
+	M = max_M;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
 			arr[i][j] = tmp[i][j];
-		}
-	}
-	for (int i = 0; i < 50; i++) {
-		for (int j = 0; j < 101; j++) {
-			int t = arr[i][j];
-			arr[i][j] = arr[100 - i][j];
-			arr[100 - i][j] = t;
-		}
-	}
 }
 
-void turnRight() {
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++) {
-			tmp[j][100 - i] = arr[i][j];
-			arr[i][j] = 0;
+void C_operator() {
+	int max_N = 0;
+	for (int j = 0; j < M; j++) {
+		map<int, int> m;
+		for (int i = 0; i < N; i++) {
+			if (!arr[i][j]) continue;
+			m[arr[i][j]]++;
 		}
+		set<Point> s;
+		for (auto& it : m) s.insert({ it.first, it.second });
+		int i = 0;
+		for (auto& it : s) {
+			tmp[i++][j] = it.num;
+			tmp[i++][j] = it.cnt;
+			if (i == 100) break;
+		}
+		max_N = max(max_N, i);
 	}
-}
-
-void turnLeft() {
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++) {
-			tmp[100 - j][i] = arr[i][j];
-		}
-	}
-}
-
-void counting(int flag) {
-	if (flag == 1) maxC = 0;
-	else if (flag == 2) maxR = 0;
-
-	for (int i = 0; i < 101; i++) {
-		clearTmp();
-		for (int j = 0; j < 101; j++) {
-			countArr[tmp[i][j]]++;
-		}
-		for (int j = 1; j < 101; j++) {
-			if (countArr[j] != 0)
-				pq.push({ j, countArr[j] });
-		}
-		int cnt = 0;
-		int size = pq.size() * 2;
-		// R
-		if (flag == 1)
-			maxC = maxC < size ? size : maxC;
-		// C
-		else if (flag == 2)
-			maxR = maxR < size ? size : maxR;
-
-		while (!pq.empty()) {
-			arr[i][cnt++] = pq.top().num;
-			arr[i][cnt++] = pq.top().cnt;
-			pq.pop();
-		}
-	}
-}
-
-void cal() {
-	while (sec <= 100) {
-		if (arr[row - 1][colum - 1] == k)
-			return;
-		for (int i = 0; i < 101; i++) {
-			for (int j = 0; j < 101; j++) {
-				tmp[i][j] = 0;
-			}
-		}
-
-		if (maxR >= maxC) {
-			copyArrA();
-			counting(1);
-		}
-		else {
-			turnRight();
-			counting(2);
-			turnLeft();
-			copyArrB();
-		}
-		sec++;
-	}
-	sec = -1;
+	N = max_N;
+	for (int j = 0; j < M; j++)
+		for (int i = 0; i < N; i++)
+			arr[i][j] = tmp[i][j];
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> row >> colum >> k;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cin >> arr[i][j];
-		}
+	scanf("%d %d %d", &r, &c, &k);
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			scanf(" %d", &arr[i][j]);
+	while (t <= 100) {
+		if (arr[r - 1][c - 1] == k) break;
+		memset(tmp, 0, sizeof(tmp));
+		if (N >= M) R_operator();
+		else C_operator();
+		t++;
 	}
-	cal();
-	cout << sec << '\n';
-
+	if (t == 101) t = -1;
+	printf("%d\n", t);
 	return 0;
 }
