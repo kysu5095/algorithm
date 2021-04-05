64c42a1a502863d962ef4ac32908dcdfc154441b
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-05 19:34:45 +0900

    [Gold 3] 경사로

--- baekJoon/baekjoon_14890.cpp
+++ baekJoon/baekjoon_14890.cpp
@@ -1,128 +1,32 @@
-//  경사로  //
-/////////////
+// 경사로
 
-#include <iostream>
-#include <cstring>
-#include <cmath>
+#include <stdio.h>
 
 using namespace std;
 
-int n, l;   // 2 <= n <= 100,  1 <= l <= n
-int route = 0;
-int arr[101][101][2];
-int arr2[101][101][2];
-
-class Point {
-public:
-	int x, y;
-};
-Point p[2] = { {1, 0}, {0, 1} }; // 오른쪽 or 아래쪽만 이동 가능
-
-void copyArr() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			arr[i][j][0] = arr2[i][j][0];
-			arr[i][j][1] = arr2[i][j][1];
-		}
-	}
-}
-
-// 앞쪽에 벽 세우기
-bool foreWall(int x, int y, int flag) {
-	for (int i = 0; i < l; i++) {
-		if (x + (p[flag].x * i) >= n || y + (p[flag].y * i) >= n) return false; // 범위초과
-		if (arr[y][x][0] - arr[y + (p[flag].y * i)][x + (p[flag].x * i)][0] != 0) return false; // 높이 일정치 않음
-	}
-	// 해당칸에 경사로 있다는걸 저장
-	for (int i = 0; i < l; i++) {
-		arr[y + (p[flag].y * i)][x + (p[flag].x * i)][1] = 1;
-	}
-
-	return true;
-}
-
-// 뒷쪽에 벽 세우기
-bool backWall(int x, int y, int flag) {
-	for (int i = 0; i < l; i++) {
-		if (x - (p[flag].x * i) < 0 || y - (p[flag].y * i) < 0) return false; // 범위초과
-		if (arr[y - (p[flag].y * i)][x - (p[flag].x * i)][0] - arr[y][x][0] != 0) return false; // 높이 일정치 않음
-		if (arr[y - (p[flag].y * i)][x - (p[flag].x * i)][1] == 1) return false; // 경사로 있음
-	}
-	return true;
-}
-
-// 가로 보기
-bool inclineW(int cnt) {
-	int flag = 0;
-	int isIncline = 0;
-	for (int i = 0; i < n - 1; i++) {
-		if (abs(arr[cnt][i][0] - arr[cnt][i + 1][0]) > 1) return false; // 차이 2이상 return
-
-		if (arr[cnt][i][0] - arr[cnt][i + 1][0] < 0) { // 뒷칸이 더 높을경우
-			// 높이 일정치 않을경우
-			if (!backWall(i, cnt, flag)) 
-				return false;
-		}
-
-		else if (arr[cnt][i][0] - arr[cnt][i + 1][0] > 0) { // 앞칸이 더 높을경우
-			if (foreWall(i + 1, cnt, flag)) {
-				if (l == 1) continue;
-				i += (l - 1);
-			}
-			// 높이 일정치 않을경우
-			else
-				return false;
-		}
-	}
-	return true;
-}
-
-// 세로 보기
-bool inclineH(int cnt) {
-	int flag = 1;
-	int isIncline = 0;
-	for (int i = 0; i < n - 1; i++) {
-		if (abs(arr[i][cnt][0] - arr[i + 1][cnt][0]) > 1) return false; // 차이 2이상 return
-
-		if (arr[i][cnt][0] - arr[i + 1][cnt][0] < 0) { // 뒷칸이 더 높을경우
-			// 높이 일정치 않을경우
-			if (!backWall(cnt, i, flag))
-				return false;
-		}
-
-		else if (arr[i][cnt][0] - arr[i + 1][cnt][0] > 0) { // 앞칸이 더 높을경우
-			if (foreWall(cnt, i + 1, flag)) {
-				if (l == 1) continue;
-				i += (l - 1);
-			}
-			// 높이 일정치 않을경우
-			else
-				return false;
-		}
-	}
-	return true;
-}
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
+	int n, l, res = 0;
+	int arr[101][202];
+	scanf("%d %d", &n, &l);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			scanf(" %d", &arr[i][j]);
 
-	cin >> n >> l;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> arr2[i][j][0];
-			arr2[i][j][1] = 0;
+	for (int j = 0; j < n; j++)
+		for (int i = 0; i < n; i++)
+			arr[j][i + n] = arr[i][j];
+
+	int i, cnt;
+	for (int j = 0; j < n + n; j++) {
+		cnt = 1;
+		for (i = 0; i < n - 1; i++) {
+			if (arr[i][j] == arr[i + 1][j]) cnt++;
+			else if (arr[i][j] + 1 == arr[i + 1][j] && cnt >= l) cnt = 1;
+			else if (arr[i][j] - 1 == arr[i + 1][j] && cnt >= 0) cnt = 1 - l;
+			else break;
 		}
+		if (i == n - 1 && cnt >= 0)	res++;
 	}
-
-	for (int i = 0; i < n; i++) {
-		copyArr();
-		if (inclineW(i)) route++;
-		copyArr();
-		if (inclineH(i)) route++;
-	}
-
-	cout << route << '\n';
-
+	printf("%d\n", res);
 	return 0;
 }
