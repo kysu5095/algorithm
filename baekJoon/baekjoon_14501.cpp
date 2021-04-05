506dda71c2149112df315a2c2b5b8ad4ad0a371c
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-05 19:52:26 +0900

    [Silver 4] 퇴사

--- baekJoon/baekjoon_14501.cpp
+++ baekJoon/baekjoon_14501.cpp
@@ -1,74 +1,24 @@
-// 퇴사 //
-/////////
+// 퇴사
 
-#include <iostream>
+#include <stdio.h>
+#include <algorithm>
 
-using namespace std;
+int n, res = 0;
+int arr[15][2];
 
-int n, maxP = 0;
-class Day {
-public:
-	int t, p;
-};
-Day day[16];
-
-void getMax(int val) {
-	maxP = maxP > val ? maxP : val;
-}
-
-void work(int idx, int val) {
-	if (idx > n) return;
-
-	getMax(val);
-	for (int i = idx; i < n; i++) {
-		work(i + day[i].t, val + day[i].p);
+void dfs(int day, int total) {
+	if (day >= n) {
+		res = res < total ? total : res;
+		return;
 	}
+	if(day + arr[day][0] <= n) dfs(day + arr[day][0], total + arr[day][1]);
+	dfs(day + 1, total);
 }
 
 int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int t, p;
-		cin >> t >> p;
-		day[i].t = t;
-		day[i].p = p;
-	}
-	work(0, 0);
-	cout << maxP << '\n';
-
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf(" %d %d", &arr[i][0], &arr[i][1]);
+	dfs(0, 0);
+	printf("%d\n", res);
 	return 0;
 }
-
-// #include <iostream>
-
-// using namespace std;
-
-// int n, max_value = 0;
-// int arr[16][2] = { {0, }, };
-// bool visited[16] = { false, };
-
-// void resign(int cnt, int sum) {
-// 	for (int i = cnt; i < n; i++) {
-// 		if (!visited[i] && arr[i][0] + i - 1 < n) {
-// 			visited[i] = true;
-// 			sum += arr[i][1];
-// 			max_value = max_value < sum ? sum : max_value;
-// 			resign(i + arr[i][0], sum);
-// 			visited[i] = false;
-// 			sum -= arr[i][1];
-// 		}
-// 	}
-// }
-
-// int main() {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(NULL);
-// 	cin >> n;
-// 	for (int i = 0; i < n; i++) {
-// 		cin >> arr[i][0] >> arr[i][1];
-// 	}
-// 	resign(0, 0);
-// 	cout << max_value << '\n';
-
-// 	return 0;
-// }
