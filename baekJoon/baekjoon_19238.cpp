8b38ee19844baaae02b198d9927636a509e6e6ad
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-06 02:11:46 +0900

    [Gold 4] 스타트 택시

--- baekJoon/baekjoon_19238.cpp
+++ baekJoon/baekjoon_19238.cpp
@@ -1,157 +1,131 @@
 // 스타트 택시
 
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
+#include <stdio.h>
 #include <string.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
-int n, m, f, ty, tx;
+int n, M, k;
 int arr[21][21];
-bool visited[21][21] = { {false,}, };
+bool visited[21][21];
 int dy[4] = { 0, 0, 1, -1 };
 int dx[4] = { 1, -1, 0, 0 };
-pair<int, int> taxi[404];
+map<pair<int, int>, pair<int, int>> m;
+vector<pair<int, int>> v;
+queue<pair<int, int>> q;
 
-class Point {
-public:
-	int y, x;
-	bool operator < (const Point& p) const {
-		if (y > p.y) return false;
-		else if (y < p.y) return true;
-		else {
-			if (x > p.x) return false;
-			else return true;
+void init() {
+	while (!q.empty()) q.pop();
+	v.clear();
+	memset(visited, false, sizeof(visited));
+}
+
+bool find_customer(int& y, int& x, int& k) {
+	int ny, nx, len;
+	init();
+	q.push(make_pair(y, x));
+	visited[y][x] = true;
+
+	while (!q.empty() && k) {
+		len = q.size();
+		while (len--) {
+			y = q.front().first;
+			x = q.front().second;
+			q.pop();
+
+			if (m.count(make_pair(y, x))) v.push_back(make_pair(y, x));
+			else {
+				for (int i = 0; i < 4; i++) {
+					ny = y + dy[i];
+					nx = x + dx[i];
+					if (ny < 0 || ny == n || nx < 0 || nx == n || arr[ny][nx] || visited[ny][nx]) continue;
+					q.push(make_pair(ny, nx));
+					visited[ny][nx] = true;
+				}
+			}
+		}
+		if (v.size()) break;
+		k--;
+	}
+
+	if (v.empty()) {
+		k = -1;
+		return false;
+	}
+	return true;
+}
+
+bool find_destination(int& y, int& x, int& k) {
+	int sy = y, sx = x;
+	int ny, nx, len, distance = 0;
+	init();
+	q.push(make_pair(y, x));
+	visited[y][x] = true;
+
+	while (!q.empty() && k) {
+		k--;
+		distance++;
+		len = q.size();
+		while (len--) {
+			y = q.front().first;
+			x = q.front().second;
+			q.pop();
+			for (int i = 0; i < 4; i++) {
+				ny = y + dy[i];
+				nx = x + dx[i];
+				if (ny < 0 || ny == n || nx < 0 || nx == n || arr[ny][nx] || visited[ny][nx]) continue;
+				if (m[make_pair(sy, sx)] == make_pair(ny, nx)) {
+					y = ny;
+					x = nx;
+					k += distance * 2;
+					m.erase(make_pair(sy, sx));
+					return true;
+				}
+				q.push(make_pair(ny, nx));
+				visited[ny][nx] = true;
+			}
 		}
 	}
-};
 
-void bfs() {
-	queue<pair<int, int>> q;
-	vector<Point> v;
-	int y, x, ny, nx, sy, sx, fy, fx, len, cnt;
-	bool flag;
+	k = -1;
+	return false;
+}
+
+void taxi(int y, int x) {
+	int ny, nx, len, distance;
 	while (true) {
-		q.push(make_pair(ty, tx));
-		visited[ty][tx] = true;
-		// 출발지 찾기
-		while (!q.empty()) {
-			len = q.size();
-			while (len--) {
-				y = q.front().first;
-				x = q.front().second;
-				q.pop();
-				if (arr[y][x] > 1 && arr[y][x] < 500)
-					v.push_back({ y, x });
-				for (int i = 0; i < 4; i++) {
-					ny = y + dy[i];
-					nx = x + dx[i];
-					if (ny < 0 || ny == n || nx < 0 || nx == n) continue;
-					if (arr[ny][nx] == 1 || visited[ny][nx]) continue;
-					visited[ny][nx] = true;
-					q.push(make_pair(ny, nx));
-				}
+		if (!find_customer(y, x, k)) break;
+		sort(v.begin(), v.end(), [](pair<int, int>& p1, pair<int, int>& p2) {
+			if (p1.first < p2.first) return true;
+			else if(p1.first == p2.first) {
+				if (p1.second < p2.second) return true;
+				else return false;
 			}
-			// 찾음
-			if (v.size()) break;
-			f--;
-			if (f == 0) {
-				f = -1;
-				return;
-			}
-		}
-		// 출발지까지 갈 수 없음
-		if (v.empty()) {
-			f = -1;
-			return;
-		}
-		sort(v.begin(), v.end());
-		sy = v[0].y;
-		sx = v[0].x;
-		fy = taxi[arr[sy][sx]].first;
-		fx = taxi[arr[sy][sx]].second;
-		arr[sy][sx] = 0;
-
-		// 초기화
-		while (!q.empty()) q.pop();
-		memset(visited, false, sizeof(visited));
-		q.push(make_pair(sy, sx));
-		visited[sy][sx] = true;
-		cnt = 1;
-		flag = false;
-
-		// 도착지 찾기
-		while (!q.empty()) {
-			len = q.size();
-			while (len-- && !flag) {
-				y = q.front().first;
-				x = q.front().second;
-				q.pop();
-				for (int i = 0; i < 4; i++) {
-					ny = y + dy[i];
-					nx = x + dx[i];
-					if (ny < 0 || ny == n || nx < 0 || nx == n) continue;
-					if (arr[ny][nx] == 1 || visited[ny][nx]) continue;
-					// 도착지 찾음
-					if (ny == fy && nx == fx) {
-						f--;
-						f += (cnt << 1);
-						ty = ny;
-						tx = nx;
-						//arr[ny][nx] = 0;
-						m--;
-						flag = true;
-						break;
-					}
-					visited[ny][nx] = true;
-					q.push(make_pair(ny, nx));
-				}
-			}
-			if (flag) break;
-			cnt++;
-			f--;
-			if (f == 0) {
-				f = -1;
-				return;
-			}
-		}
-		// 도착지 못찾음
-		if (!flag) {
-			f = -1;
-			return;
-		}
-		// 모든 승객 이동 완료
-		if (m == 0) break;
-		while (!q.empty()) q.pop();
-		v.clear();
-		memset(visited, false, sizeof(visited));
+			else return false;
+		});
+		y = v[0].first, x = v[0].second;
+		if (!find_destination(y, x, k)) break;
+		if (m.empty()) break;
 	}
 }
 
 int main() {
-	cin.sync_with_stdio(0);
-	cin.tie(0);
-	int sy, sx, fy, fx;
-	cin >> n >> m >> f;
-
+	scanf("%d %d %d", &n, &M, &k);
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			cin >> arr[i][j];
-	cin >> ty >> tx;
-	ty--;
-	tx--;
-	int idx = 2;
-	for (int i = 0; i < m; i++) {
-		cin >> sy >> sx >> fy >> fx;
-		arr[sy - 1][sx - 1] = idx;
-		taxi[idx].first = fy - 1;
-		taxi[idx].second = fx - 1;
-		idx++;
+			scanf(" %d", &arr[i][j]);
+	int y, x;
+	scanf(" %d %d", &y, &x);
+	for (int i = 0; i < M; i++) {
+		int y1, x1, y2, x2;
+		scanf(" %d %d %d %d", &y1, &x1, &y2, &x2);
+		m[make_pair(y1 - 1, x1 - 1)] = make_pair(y2 - 1, x2 - 1);
 	}
-	bfs();
-	cout << f << '\n';
+	taxi(y - 1, x - 1);
+	printf("%d\n", k);
 	return 0;
 }
-
