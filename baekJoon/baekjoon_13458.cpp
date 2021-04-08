78e70eccc3dc86eb5a0670f80858bca93127a610
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-08 12:25:33 +0900

    [Bronze 2] 시험 감독

--- baekJoon/baekjoon_13458.cpp
+++ baekJoon/baekjoon_13458.cpp
@@ -1,71 +1,23 @@
-// 시험감독 //
-/////////////
+// 시험 감독
 
-#include <iostream>
-
-using namespace std;
-
-int n, b, c;
-long long cnt = 0;
-int arr[1000001] = { 0, };
-
-void find() {
-	for (int i = 0; i < n; i++) {
-		cnt++;
-		int tmp = arr[i] - b;
-		if (tmp <= 0) continue;
-
-		if (tmp % c == 0) cnt += (tmp / c);
-		else cnt += (tmp / c) + 1;
-	}
-}
+#include <stdio.h>
+#define ll long long
+int arr[1000001];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> n;
+	int n;
+	ll master = 0, slave = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf(" %d", &arr[i]);
+	int b, c;
+	scanf(" %d %d", &b, &c);
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+		arr[i] -= b;
+		master++;
+		if (arr[i] <= 0) continue;
+		if (arr[i] % c > 0) slave += arr[i] / c + 1;
+		else slave += arr[i] / c;
 	}
-	cin >> b >> c;
-	find();
-	cout << cnt << '\n';
-
+	printf("%lld\n", master + slave);
 	return 0;
 }
-
-// #include<iostream>
-
-// using namespace std;
-// int course[1000001] = { 0, };
-// int room, supervisor, deputy;
-// long long min_supervisor = 0;
-
-// void find() {
-// 	for (int i = 0; i < room; i++) {
-// 		int cnt_supervisor = 1;
-// 		int cnt_deputy = 0;
-// 		course[i] -= supervisor;
-// 		if (course[i] > 0) {
-// 			if (course[i] % deputy == 0)
-// 				cnt_deputy = course[i] / deputy;
-// 			else
-// 				cnt_deputy = course[i] / deputy + 1;
-// 		}
-// 		min_supervisor += (cnt_supervisor + cnt_deputy);
-// 	}
-// }
-
-// int main() {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cin >> room;
-// 	for (int i = 0; i < room; i++) {
-// 		cin >> course[i];
-// 	}
-// 	cin >> supervisor >> deputy;
-// 	find();
-// 	cout << min_supervisor << '\n';
-
-// 	return 0;
-// }
