5ff08d68db62f0707111049dcfd1d80ca98cad71
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-09 14:24:26 +0900

    [Gold 2] 주사위 윷놀이_실패

--- baekJoon/baekjoon_17825.cpp
+++ baekJoon/baekjoon_17825.cpp
@@ -1,182 +1,225 @@
-// 주사위 윷놀이 //
-//////////////////
+// 주사위 윷놀이
 
-/////////////////////////////////////////////////
-/// 맵을 직접 만드는게 편하다 함
-/////////////////////////////////////////////////
-
-#include <iostream>
-
+#include <stdio.h>
+#include <vector>
 using namespace std;
 
-int result = 0;
-int turn[10] = { 0, };
-int red_line[21];
-int blue_line1[8] = { 10, 13, 16, 19, 25, 30, 35, 40 };
-int blue_line2[7] = { 20, 22, 24, 25, 30, 35, 40 };
-int blue_line3[8] = { 30, 28, 27, 26, 25, 30, 35, 40 };
-
-// 말의 대한 정보
-class Dice {
-public:
-	// where : 어떤 라인에 있는지 (0 : red_line, 1 : blue_line1 ,,,)
-	// cnt : 해당 라인의 몇번째 칸에 있는지
-	// value : 현재 해당말이 있는 점수판의 점수
-	int where = 0, cnt = 0, value = 0;
+int res = 0;
+int path_1[21], path_2[5] = { 10,13,16,19,25 }, path_3[4] = { 20,22,24,25 }, path_4[5] = { 30,28,27,26,25 };
+int path_5[4] = { 25,30,35,40 }; int path_6[1] = { 40 };
+int command[10];
+struct Point {
+	bool end;
+	int path, idx;
 };
-Dice dice[4];
 
-// 말이 움직일 수 있는지 검사
-// horse : 움직일 말의 번호
-// move_cnt : 움직일 칸의 개수
-// sum : 현재까지의 점수
-bool move_horse(int horse, int move_cnt, int& sum) {
-	int where = dice[horse].where;
-	int cnt = dice[horse].cnt;
-	int value = dice[horse].value;
+//int move(int go, Point& p) {
+//	int ret = 0;
+//	p.idx += go;
+//	switch (p.path)
+//	{
+//	case 1:
+//		if (p.idx > 20) {
+//			p.end = true;
+//			break;
+//		}
+//		ret = path_1[p.idx];
+//		if (p.idx == 5) {
+//			p.path = 2;
+//			p.idx = 0;
+//		}
+//		else if (p.idx == 10) {
+//			p.path = 3;
+//			p.idx = 0;
+//		}
+//		else if (p.idx == 15) {
+//			p.path = 4;
+//			p.idx = 0;
+//		}
+//		else if (p.idx == 20) {
+//			p.path = 6;
+//			p.idx = 0;
+//		}
+//		break;
+//	case 2:
+//		if (p.idx == 8) {
+//			p.end = true;
+//			break;
+//		}
+//		else if (p.idx >= 4) {
+//			p.path = 5;
+//			p.idx = p.idx - 4;
+//			ret = path_5[p.idx];
+//		}
+//		else ret = path_2[p.idx];
+//		break;
+//	case 3:
+//		if (p.idx >= 7) {
+//			p.end = true;
+//			break;
+//		}
+//		else if (p.idx >= 3) {
+//			p.path = 5;
+//			p.idx = p.idx - 3;
+//			ret = path_5[p.idx];
+//		}
+//		else ret = path_3[p.idx];
+//		break;
+//	case 4:
+//		if (p.idx == 8) {
+//			p.end = true;
+//			break;
+//		}
+//		else if (p.idx >= 4) {
+//			p.path = 5;
+//			p.idx = p.idx - 4;
+//			ret = path_5[p.idx];
+//		}
+//		else ret = path_4[p.idx];
+//		break;
+//	case 5:
+//		if (p.idx > 3) {
+//			p.end = true;
+//			break;
+//		}
+//		ret = path_5[p.idx];
+//		if (p.idx == 3) {
+//			p.path = 6;
+//			p.idx = 0;
+//		}
+//		break;
+//	case 6:
+//		p.end = true;
+//		break;
+//	}
+//	return ret;
+//}
 
-	if (where == 0) { // red_line
-		// 현재위치 + 움직일 칸 >= 말판의 끝
-		if (cnt + move_cnt >= 21) {
-			dice[horse].where = 4;
-			dice[horse].value = 99;
-			return true;
+bool possible(Point p[4]) {
+	for (int i = 0; i < 4; i++) {
+		if (p[i].end) continue;
+		int path = p[i].path;
+		int idx = p[i].idx;
+		if (path == 1 && idx == 0) continue;
+		for (int j = 0; j < 4; j++) {
+			if (i == j) continue;
+			if (p[j].end) continue;
+			if (path == p[j].path && idx == p[j].idx) return false;
 		}
-		// 이동하려는 칸에 말이 존재하면 false
-		for (int i = 0; i < 4; i++) {
-			if (i == horse)	continue;
-			if (red_line[cnt + move_cnt] == dice[i].value) {
-				if (where == dice[i].where)
-					return false;
-				if ((cnt + move_cnt) % 5 == 0)
-					return false;
-			}
-		}
-
-		sum += red_line[cnt + move_cnt];
-		// 도착한 위치가 파랑칸일 경우
-		if ((cnt + move_cnt) % 5 == 0 && (cnt + move_cnt != 20)) {
-			dice[horse].where = (cnt + move_cnt) / 5;
-			dice[horse].cnt = 0;
-		}
-		else {
-			dice[horse].cnt += move_cnt;
-		}
-		dice[horse].value = red_line[cnt + move_cnt];
-
-		return true;
 	}
-	else if (where == 1) { // blue_line1
-		// 현재위치 + 움직일 칸 >= 말판의 끝
-		if (cnt + move_cnt >= 8) {
-			dice[horse].where = 4;
-			dice[horse].value = 99;
-			return true;
-		}
-		// 이동하려는 칸에 말이 존재하면 false
-		for (int i = 0; i < 4; i++) {
-			if (i == horse)	continue;
-			if (blue_line1[cnt + move_cnt] == dice[i].value) {
-				if (dice[i].where != 0)
-					return false;
-				if (dice[i].value == 40)
-					return false;
-			}
-		}
-
-		sum += blue_line1[cnt + move_cnt];
-		dice[horse].cnt += move_cnt;
-		dice[horse].value = blue_line1[cnt + move_cnt];
-		return true;
-	}
-	else if (where == 2) { // blue_line2
-		// 현재위치 + 움직일 칸 >= 말판의 끝
-		if (cnt + move_cnt >= 7) {
-			dice[horse].where = 4;
-			dice[horse].value = 99;
-			return true;
-		}
-		// 이동하려는 칸에 말이 존재하면 false
-		for (int i = 0; i < 4; i++) {
-			if (i == horse)	continue;
-			if (blue_line2[cnt + move_cnt] == dice[i].value) {
-				if (dice[i].where != 0)
-					return false;
-				if (dice[i].value == 40)
-					return false;
-			}
-		}
-
-		sum += blue_line2[cnt + move_cnt];
-		dice[horse].cnt += move_cnt;
-		dice[horse].value = blue_line2[cnt + move_cnt];
-		return true;
-	}
-	else if (where == 3) { // blue_line3
-		// 현재위치 + 움직일 칸 >= 말판의 끝
-		if (cnt + move_cnt >= 8) {
-			dice[horse].where = 4;
-			dice[horse].value = 99;
-			return true;
-		}
-		// 이동하려는 칸에 말이 존재하면 false
-		for (int i = 0; i < 4; i++) {
-			if (i == horse)	continue;
-			if (blue_line3[cnt + move_cnt] == dice[i].value) {
-				if (dice[i].where != 0)
-					return false;
-				if (dice[i].value == 40)
-					return false;
-			}
-		}
-
-		sum += blue_line3[cnt + move_cnt];
-		dice[horse].cnt += move_cnt;
-		dice[horse].value = blue_line3[cnt + move_cnt];
-		return true;
-	}
-	else { // where == 4 >> 말판의 끝까지 도달
-		return false;
-	}
+	return true;
 }
 
-void dfs(int cnt, int sum) {
-	// 10번 이동을 마친 경우 종료
-	if (cnt == 10) {
-		result = result < sum ? sum : result;
-		return;
+int move(int go, int& idx, Point* p) {
+	while (go--) {
+		if (p[idx].end) return -1;
+		p[idx].idx++;
+		if (p[idx].path == 6 && p[idx].idx > 0) {
+			p[idx].end = true;
+			return 0;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			if (idx == i || p[idx].end) continue;
+			if (p[idx].path == 1) {
+				if (p[idx].idx == 5  && p[i].path == 2 && p[i].idx == 0) return -1;
+				if (p[idx].idx == 10 && p[i].path == 3 && p[i].idx == 0) return -1;
+				if (p[idx].idx == 15 && p[i].path == 4 && p[i].idx == 0) return -1;
+				if (p[idx].idx == 20 && p[i].path == 6 && p[i].idx == 0) return -1;
+				if (p[idx].idx == 20) {
+					p[idx].path = 6;
+					p[idx].idx = 0;
+				}
+			}
+			else if (p[idx].path == 2) {
+				if (p[idx].idx == 4) {
+					p[idx].path = 5;
+					p[idx].idx = 0;
+				}
+			}
+			else if (p[idx].path == 3) {
+				if (p[idx].idx == 3) {
+					p[idx].path = 5;
+					p[idx].idx = 0;
+				}
+			}
+			else if (p[idx].path == 4) {
+				if (p[idx].idx == 4) {
+					p[idx].path = 5;
+					p[idx].idx = 0;
+				}
+			}
+			else if (p[idx].path == 5) {
+				if (p[idx].idx == 3) {
+					p[idx].path = 6;
+					p[idx].idx = 0;
+				}
+			}
+			if (p[idx].path == p[i].path && p[idx].idx == p[i].idx) return -1;
+		}
+	}
+	if (p[idx].path == 1) {
+		if (p[idx].idx == 5) {
+			p[idx].path = 2;
+			p[idx].idx = 0;
+		}
+		else if (p[idx].idx == 10) {
+			p[idx].path = 3;
+			p[idx].idx = 0;
+		}
+		else if (p[idx].idx == 15) {
+			p[idx].path = 4;
+			p[idx].idx = 0;
+		}
 	}
 
-	//// 모든 말이 말판의 끝에 도달해 있을 경우 종료
-	//for (int i = 0; i < 4; i++) {
-	//	if (dice[i].where == 4 && i == 3) {
-	//		result = result < sum ? sum : result;
-	//		return;
-	//	}
-	//}
-	int tmp = sum;
-	for (int i = 0; i < 4; i++) {
-		Dice tmp_dice = dice[i];
-		if (move_horse(i, turn[cnt], sum)) {
-			dfs(cnt + 1, sum);
-			// dfs 이전상태로 초기화
-			sum = tmp;
-			dice[i] = tmp_dice;
+	if      (p[idx].path == 1) return path_1[p[idx].idx];
+	else if (p[idx].path == 2) return path_2[p[idx].idx];
+	else if (p[idx].path == 3) return path_3[p[idx].idx];
+	else if (p[idx].path == 4) return path_4[p[idx].idx];
+	else if (p[idx].path == 5) return path_5[p[idx].idx];
+	else if (p[idx].path == 6) return path_6[p[idx].idx];
+}
+
+void dfs(int cnt, int total, Point p[4], vector<pair<pair<int, int>, pair<int, int>>> v[4]) {
+	if (cnt == 10) {
+		if (res < total) {
+			res = total;
+			printf("\ntotal : %d\n", total);
+			for (int i = 0; i < 4; i++) {
+				printf("%dth path\n", i);
+				for (auto& node : v[i]) printf("(turn%d(%d) path : %d, idx : %d, score : %d)\n", node.first.first, command[node.first.first], node.second.first, node.second.second, node.first.second);
+				printf("\n");
+			}
 		}
+		//res = res < total ? total : res;
+		return;
+	}
+	Point tmp;
+	for (int i = 0; i < 4; i++) {
+		if (p[i].end) continue;
+		Point tmp = p[i];
+		int score = move(command[cnt], i, p);
+		if (score >= 0) {
+			v[i].push_back(make_pair(make_pair(cnt, score), make_pair(p[i].path, p[i].idx)));
+			dfs(cnt + 1, total + score, p, v);
+			v[i].pop_back();
+		}
+		p[i] = tmp;
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	for (int i = 0; i <= 20; i++)
-		red_line[i] = i * 2;
-
-	for (int i = 0; i < 10; i++) {
-		cin >> turn[i];
+	for (int i = 0; i < 10; i++) scanf(" %d", &command[i]);
+	for (int i = 1; i < 21; i++) path_1[i] = i * 2;
+	Point p[4];
+	vector<pair<pair<int, int>, pair<int, int>>> v[4];
+	for (int i = 0; i < 4; i++) {
+		p[i].end = false;
+		p[i].path = 1;
+		p[i].idx = 0;
 	}
-	dfs(0, 0);
-
-	cout << result << '\n';
+	dfs(0, 0, p, v);
+	printf("%d\n", res);
 	return 0;
 }
