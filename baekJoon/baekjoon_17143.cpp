f24abffcad472a3786cfc64b9eeacaa626f9df49
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-10 11:09:22 +0900

    [Gold 2] 낚시왕

--- baekJoon/baekjoon_17143.cpp
+++ baekJoon/baekjoon_17143.cpp
@@ -1,290 +1,91 @@
-// 낚시왕 //
-///////////
+// 낚시왕
 
-#include <iostream>
+#include <stdio.h>
 #include <queue>
+#include <vector>
+#include <algorithm>
+#include <string.h>
 
 using namespace std;
 
-int r, c, m, sum = 0;
-int arr[102][102] = { {0, }, };
+int r, c, m;
+int arr[101][101], tmp[101][101];
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
 
-class Point {
-public:
-	int y, x, speed, direct, size;
-
-	bool operator < (const Point& p) const {
-		if (this->size < p.size) return true;
-		else return false;
-	}
+struct SHARK {
+	bool die;
+	int y, x, s, d, z;
 };
-Point p[4] = { {-1, 0}, {1, 0}, {0, 1}, {0, -1} };
-deque<Point> dq[102][102];
+vector<SHARK> v;
 
-void turnDirect(int& direct) {
-	if (direct == 0) direct = 1;
-	else if (direct == 1) direct = 0;
-	else if (direct == 2) direct = 3;
-	else if (direct == 3) direct = 2;
-}
+void move_shark() {
+	memset(tmp, -1, sizeof(tmp));
+	for (int idx = 0; idx < m; idx++) {
+		if (v[idx].die) continue;
+		int y = v[idx].y;
+		int x = v[idx].x;
+		int s = v[idx].s;
+		int d = v[idx].d;
 
-void eatShark(priority_queue<Point> pq[][102]) {
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			if (!pq[i][j].empty()) {
-				dq[i][j].push_back(pq[i][j].top());
-				arr[i][j] = 1;
+		if (d < 2) s %= (2 * (r - 1));
+		else s %= (2 * (c - 1));
+
+		for (int i = 0; i < s; i++) {
+			y += dy[d];
+			x += dx[d];
+			if (y < 0 || y == r || x < 0 || x == c) {
+				if (d % 2) d--;
+				else d++;
+				y += (dy[d] * 2);
+				x += (dx[d] * 2);
 			}
 		}
+		if (tmp[y][x] >= 0) {
+			v[idx].die = true;
+			continue;
+		}
+		tmp[y][x] = idx;
+		v[idx].y = y;
+		v[idx].x = x;
+		v[idx].d = d;
 	}
+
+	for (int i = 0; i < r; i++)
+		for (int j = 0; j < c; j++)
+			arr[i][j] = tmp[i][j];
 }
 
-void moveShark() {
-	priority_queue<Point> pq[102][102];
-
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			if (arr[i][j] == 1) {
-				int x = j;
-				int y = i;
-				int speed = dq[i][j].back().speed;
-				int direct = dq[i][j].back().direct;
-				int size = dq[i][j].back().size;
-				int tmp = speed;
-				if (direct == 0 || direct == 1) tmp %= (2 * (r - 1));
-				if (direct == 2 || direct == 3) tmp %= (2 * (c - 1));
-
-				while (tmp > 0) {
-					x += p[direct].x;
-					y += p[direct].y;
-					if (x < 0 || x >= c || y < 0 || y >= r) {
-						turnDirect(direct);
-						x += (2 * p[direct].x);
-						y += (2 * p[direct].y);
-					}
-					tmp--;
-				}
-				arr[i][j] = 0;
-				dq[i][j].pop_back();
-				pq[y][x].push({ y, x, speed, direct, size });
+int catch_shark() {
+	int ret = 0;
+	for (int x = 0; x < c; x++) {
+		for (int y = 0; y < r; y++) {
+			if (arr[y][x] >= 0) {
+				ret += v[arr[y][x]].z;
+				v[arr[y][x]].die = true;
+				arr[y][x] = -1;
+				break;
 			}
 		}
+		move_shark();
 	}
-	eatShark(pq);
+	return ret;
 }
 
-void catchShark(int colum) {
-	for (int i = 0; i < r; i++) {
-		if (!dq[i][colum].empty()) {
-			sum += dq[i][colum].front().size;
-			m--;
-			arr[i][colum] = 0;
-			dq[i][colum].pop_back();
-			return;
-		}
-	}
-}
-
-void movePerson() {
-	for (int i = 0; i < c; i++) {
-		if (m == 0) return;
-
-		catchShark(i);
-		moveShark();
-	}
-}
-	
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> r >> c >> m;
+	scanf("%d %d %d", &r, &c, &m);
+	v.resize(m);
+	memset(arr, -1, sizeof(arr));
 	for (int i = 0; i < m; i++) {
-		int sr, sc, s, d, z;
-		cin >> sr >> sc >> s >> d >> z;
-		dq[sr - 1][sc - 1].push_back({ sr - 1, sc - 1, s, d - 1, z });
-		arr[sr - 1][sc - 1] = 1;
+		int y, x, s, d, z;
+		scanf(" %d %d %d %d %d", &y, &x, &s, &d, &z);
+		v[i] = { false, y - 1, x - 1, s, d - 1, z };
 	}
-	movePerson();
-	cout << sum << '\n';
+	sort(v.begin(), v.end(), [](SHARK& s1, SHARK& s2) {
+		return s1.z > s2.z;
+	});
+	for (int i = 0; i < m; i++) arr[v[i].y][v[i].x] = i;
 
+	printf("%d\n", catch_shark());
 	return 0;
 }
-
-// // 낚시왕
-// // 11:45
-
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// #include <string.h>
-
-// using namespace std;
-
-// int r, c, m, res;
-// int arr[101][101] = { {0, }, };
-
-// class Point {
-// public:
-// 	int y, x, s, d, z;
-// };
-// Point p[4] = { {-1, 0}, {1, 0}, {0, 1}, {0, -1} };
-// vector<Point> shark;
-
-// bool desc(Point p1, Point p2) {
-// 	return p1.z > p2.z;
-// }
-
-// void catch_shark(int idx) {
-// 	int len = shark.size();
-// 	for (int i = 0; i < r; i++) {
-// 		if (arr[i][idx]) {
-// 			res += arr[i][idx];
-// 			for (int j = 0; j < len; j++) {
-// 				if (shark[j].y == i && shark[j].x == idx) {
-// 					shark.erase(shark.begin() + j);
-// 					return;
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-
-// void move_shark() {
-// 	int y, x, s, d, z, len;
-// 	memset(arr, 0, sizeof(arr));
-// 	sort(shark.begin(), shark.end(), desc);
-// 	len = shark.size();
-// 	for (int j = 0; j < len; j++) {
-// 		y = shark[j].y, x = shark[j].x;
-// 		s = shark[j].s, d = shark[j].d; z = shark[j].z;
-// 		if (d == 0 || d == 1) s %= ((r - 1) * 2);
-// 		else s %= ((c - 1) * 2);
-// 		for (int k = 0; k < s; k++) {
-// 			y += p[d].y;
-// 			x += p[d].x;
-// 			if (y < 0) { y = 1, d = 1; }
-// 			else if (y == r) { y = r - 2, d = 0; }
-// 			else if (x < 0) { x = 1, d = 2; }
-// 			else if (x == c) { x = c - 2, d = 3; }
-// 		}
-// 		if (arr[y][x]) {
-// 			shark.erase(shark.begin() + j);
-// 			j--;
-// 			len--;
-// 			continue;
-// 		}
-// 		arr[y][x] = z;
-// 		shark[j].y = y, shark[j].x = x;
-// 		shark[j].d = d;
-// 	}
-// }
-
-// void move() {
-// 	for (int i = 0; i < c; i++) {
-// 		catch_shark(i);
-// 		if (shark.empty()) break;
-// 		move_shark();
-// 	}
-// }
-
-// int main() {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(NULL);
-// 	res = 0;
-// 	int sr, sc, ss, sd, sz;
-// 	cin >> r >> c >> m;
-// 	for (int i = 0; i < m; i++) {
-// 		cin >> sr >> sc >> ss >> sd >> sz;
-// 		shark.push_back({ sr - 1, sc - 1, ss, sd - 1, sz });
-// 		arr[sr - 1][sc - 1] = sz;
-// 	}
-// 	move();
-// 	cout << res << '\n';
-// 	return 0;
-// }
-
-// #include <iostream>
-// #include <vector>
-// #include <string.h>
-
-// using namespace std;
-
-// int r, c, m, res = 0;
-// int arr[101][101], tmp[101][101];
-// int dy[5] = { 0, -1, 1, 0, 0 };
-// int dx[5] = { 0, 0, 0, 1, -1 };
-
-// class Shark {
-// public:
-// 	int y, x, s, d, z, idx;
-// 	bool live;
-// };
-// vector<Shark> v;
-
-// void catch_shark(int& x) {
-// 	int y = 0;
-// 	while (y != r) {
-// 		if (arr[y][x] >= 0) {
-// 			res += v[arr[y][x]].z;
-// 			v[arr[y][x]].live = false;
-// 			break;
-// 		}
-// 		y++;
-// 	}
-// }
-
-// void move_shark() {
-// 	memset(tmp, -1, sizeof(tmp));
-// 	int y, x, ny, nx, s, d, z, len = v.size();
-// 	for (int idx = 0; idx < len; idx++) {
-// 		if (!v[idx].live) continue;
-// 		y = v[idx].y; x = v[idx].x;
-// 		s = v[idx].s; d = v[idx].d; z = v[idx].z;
-// 		if (d <= 2) s %= ((r - 1) << 1);
-// 		else s %= ((c - 1) << 1);
-// 		while (s--) {
-// 			ny = y + dy[d];
-// 			nx = x + dx[d];
-// 			if (ny < 0 || ny == r || nx < 0 || nx == c) {
-// 				if (d % 2 == 0) d--;
-// 				else d++;
-// 				ny = y + dy[d];
-// 				nx = x + dx[d];
-// 			}
-// 			y = ny;
-// 			x = nx;
-// 		}
-// 		v[idx].y = y; v[idx].x = x; v[idx].d = d;
-// 		if (tmp[y][x] == -1) tmp[y][x] = idx;
-// 		else {
-// 			if (z < v[tmp[y][x]].z) v[idx].live = false;
-// 			else {
-// 				v[tmp[y][x]].live = false;
-// 				tmp[y][x] = idx;
-// 			}
-// 		}
-// 	}
-// 	for (int i = 0; i < r; i++)
-// 		for (int j = 0; j < c; j++)
-// 			arr[i][j] = tmp[i][j];
-// }
-
-// int main() {
-// 	cin.sync_with_stdio(0);
-// 	cin.tie(0);
-// 	memset(arr, -1, sizeof(arr));
-// 	int y, x, s, d, z, idx;
-// 	cin >> r >> c >> m;
-// 	for (int i = 0; i < m; i++) {
-// 		cin >> y >> x >> s >> d >> z;
-// 		v.push_back({ y - 1, x - 1, s, d, z, i, true });
-// 		arr[y - 1][x - 1] = i;
-// 	}
-// 	for (int i = 0; i < c; i++) {
-// 		catch_shark(i);
-// 		move_shark();
-// 	}
-// 	cout << res << '\n';
-// 	return 0;
-// }
