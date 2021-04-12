c0d46ef8fe3e01c7bf7e7d4d99b3ff4263153d1c
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-13 01:56:15 +0900

    [Gold 1] 전투 시뮬레이션

--- baekJoon/baekjoon_19537.cpp
+++ baekJoon/baekjoon_19537.cpp
@@ -1,100 +1,91 @@
 // 전투 시뮬레이션
 
-#include <iostream>
-#include <queue>
-#include <vector>
+#include <stdio.h>
 #include <string.h>
+#include <vector>
+#include <queue>
+#include <math.h>
 
 using namespace std;
 
 int n, h, w, m, k;
-int arr[501][501] = { {0,}, };
-int map[501][501] = { {0,}, };
-int visited[501][501] = { {0, }, };
-int slope[10];
-int dy[4] = { 0 ,0 ,1 ,-1 };
+int arr[501][501], map[501][501];
+int visited[501][501];
+int height[10];
+int dy[4] = { 0, 0, 1, -1 };
 int dx[4] = { 1, -1, 0, 0 };
 
-class Unit {
-public:
+struct Point {
 	int m, t, y, x;
 };
-vector<Unit> v;
+vector<Point> v;
 
-bool is_enermy(int& y, int& x, int& t) {
+bool is_movable(int& y, int& x, int& t) {
+	int ny, nx;
 	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
+		ny = y + dy[i];
+		nx = x + dx[i];
 		if (ny < 0 || ny == h || nx < 0 || nx == w) continue;
-		if (map[ny][nx] && v[map[ny][nx]].t != t) return true;
+		if (map[ny][nx] && t != v[map[ny][nx]].t) return false;;
 	}
-	return false;
+	return true;
 }
 
-bool move(int& u, int& _y, int& _x) {
-	if (map[_y][_x]) return false;
-	if (slope[arr[_y][_x]] == -1) return false;
-	int y = v[u].y, x = v[u].x;
-	int s = v[u].m, t = v[u].t;
+bool move(int& idx, int& a, int& b) {
+	if (height[arr[a][b]] == -1 || map[a][b]) return false;
 	priority_queue<pair<int, pair<int, int>>> pq;
-	pq.push(make_pair(s, make_pair(y, x)));
-	visited[y][x] = s;
-	int ny, nx, ns;
+	pq.push(make_pair(v[idx].m, make_pair(v[idx].y, v[idx].x)));
+	memset(visited, 0, sizeof(visited));
+	visited[v[idx].y][v[idx].x] = v[idx].m;
+
+	int y, x, ny, nx, s, t = v[idx].t;
 	while (!pq.empty()) {
-		s = pq.top().first;
 		y = pq.top().second.first;
 		x = pq.top().second.second;
+		s = pq.top().first;
 		pq.pop();
-		if (s < visited[y][x]) continue;
+		if (visited[y][x] > s) continue;
+		if (abs(y - a) + abs(x - b) > 20) continue;
 
-		/* 다음칸으로 이동가능한지 판별 */
 		for (int i = 0; i < 4; i++) {
 			ny = y + dy[i];
 			nx = x + dx[i];
-			if (ny < 0 || ny == h || nx < 0 || nx == w) continue;
-			ns = s - slope[arr[ny][nx]];
-			if (slope[arr[ny][nx]] == -1 || ns < 0) continue;
-			if (ny == _y && nx == _x) return true;
-			if (map[ny][nx] && v[map[ny][nx]].t != t) continue;
-			if (is_enermy(ny, nx, t)) continue;
-			if (ns <= visited[ny][nx]) continue;
-			pq.push(make_pair(ns, make_pair(ny, nx)));
-			visited[ny][nx] = ns;
+			if (ny < 0 || ny == h || nx < 0 || nx == w || height[arr[ny][nx]] == -1) continue;
+			if (s - height[arr[ny][nx]] < 0) continue;
+			if (map[ny][nx] && t != v[map[ny][nx]].t) continue;
+			if (ny == a && nx == b) return true;
+			if (!is_movable(ny, nx, t)) continue;
+			if (s - height[arr[ny][nx]] <= visited[ny][nx]) continue;
+			pq.push(make_pair(s - height[arr[ny][nx]], make_pair(ny, nx)));
+			visited[ny][nx] = s - height[arr[ny][nx]];
 		}
 	}
 	return false;
 }
 
 int main() {
-	cin.sync_with_stdio(0);
-	cin.tie(0);
-	int a, b, c, d;
-	cin >> n >> h >> w;
+	scanf("%d %d %d", &n, &h, &w);
 	for (int i = 0; i < h; i++)
 		for (int j = 0; j < w; j++)
-			cin >> arr[i][j];
-	for (int i = 1; i <= n; i++)
-		cin >> slope[i];
-
-	v.push_back({ 0, 0, 0, 0 }); /* 1번 index부터 시작하기위해 넣어줌 */
-	cin >> m;
+			scanf(" %d", &arr[i][j]);
+	for (int i = 1; i <= n; i++) scanf(" %d", &height[i]);
+	scanf(" %d", &m);
+	v.resize(m + 1);
 	for (int i = 1; i <= m; i++) {
-		cin >> a >> b >> c >> d;
-		v.push_back({ a, b, c, d });
-		map[c][d] = i; /* 맵에 유닛 표시 */
+		scanf(" %d %d %d %d", &v[i].m, &v[i].t, &v[i].y, &v[i].x);
+		map[v[i].y][v[i].x] = i;
 	}
-	cin >> k;
-	for (int i = 0; i < k; i++) {
-		cin >> a >> b >> c;
-		memset(visited, 0, sizeof(visited));
-		if (move(a, b, c)) { /* 명령 수행 */
-			map[b][c] = a;
-			map[v[a].y][v[a].x] = 0;
-			v[a].y = b;
-			v[a].x = c;
+	scanf(" %d", &k);
+	while (k--) {
+		int u, a, b;
+		scanf(" %d %d %d", &u, &a, &b);
+		if (move(u, a, b)) {
+			map[v[u].y][v[u].x] = 0;
+			map[a][b] = u;
+			v[u].y = a;
+			v[u].x = b;
 		}
 	}
-	for (int i = 1; i <= m; i++)
-		cout << v[i].y << " " << v[i].x << '\n';
+	for (int i = 1; i <= m; i++) printf("%d %d\n", v[i].y, v[i].x);
 	return 0;
 }
