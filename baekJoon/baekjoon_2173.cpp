e4c84a4b1a144bca3314879c752649ab2a9a5d01
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-14 17:28:39 +0900

    [Gold 3] 양파깡 만들기

--- baekJoon/baekjoon_2173.cpp
+++ baekJoon/baekjoon_2173.cpp
@@ -1,81 +1,66 @@
 // 양파깡 만들기
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <stdio.h>
 #include <limits.h>
-#define INF 1000
-
-using namespace std;
 
 int n, m;
 int arr[31][31];
+bool visited[31][31];
 int dy[4] = { 0, 1, 0, -1 };
 int dx[4] = { 1, 0, -1, 0 };
+struct Point {
+	int ret, sy, sx, fy, fx;
+}p[31];
 
-class Point {
-public:
-	int val, sy, sx, fy, fx;
-};
-vector<Point> v;
+int cut(int sy, int sx, int fy, int fx, bool flag) {
+	if (visited[sy][sx] || visited[fy][fx]) return -INT_MAX;
 
-int cut(int& sy, int& sx, int& fy, int& fx, bool flag) {
-	int y = sy, x = sx;
-	int val = arr[y][x];
-	int sequence[4] = { fx - sx, fy - sy, fx - sx, fy - sy };
+	int ret = 0;
+	int move_cnt[4] = { fx - sx, fy - sy, fx - sx, fy - sy };
 	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < sequence[i]; j++) {
-			y += dy[i];
-			x += dx[i];
-			if (arr[y][x] == INF) return -INT_MAX;
-			if(flag) val += arr[y][x];
-			else arr[y][x] = INF;
+		for (int j = 0; j < move_cnt[i]; j++) {
+			sy += dy[i];
+			sx += dx[i];
+			if (visited[sy][sx]) return -INT_MAX;
+			if (flag) ret += arr[sy][sx];
+			else visited[sy][sx] = true;
 		}
 	}
-	val -= arr[sy][sx];
-	return val;
+	return ret;
 }
 
-bool find() {
+bool find(int idx) {
 	int ret = -INT_MAX;
-	Point p;
-	for (int sy = 1; sy <= n - 2; sy++) {
-		for (int sx = 1; sx <= n - 2; sx++) {
-			for (int fy = sy + 2; fy <= n; fy++) {
-				for (int fx = sx + 2; fx <= n; fx++) {
+	for (int sy = 0; sy < n - 2; sy++) {
+		for (int sx = 0; sx < n - 2; sx++) {
+			for (int fy = sy + 2; fy < n; fy++) {
+				for (int fx = sx + 2; fx < n; fx++) {
 					int val = cut(sy, sx, fy, fx, true);
 					if (ret < val) {
 						ret = val;
-						p.val = val;
-						p.sy = sy, p.sx = sx;
-						p.fy = fy, p.fx = fx;
+						p[idx] = { ret, sy, sx, fy, fx };
 					}
 				}
 			}
 		}
 	}
-	if (ret != -INT_MAX) {
-		v.push_back(p);
-		cut(p.sy, p.sx, p.fy, p.fx, false);
-		return true;
-	}
-	else return false;
+	if (ret == -INT_MAX) return false;
+	cut(p[idx].sy, p[idx].sx, p[idx].fy, p[idx].fx, false);
+	return true;
 }
 
 int main() {
-	cin.sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			cin >> arr[i][j];
-	while (m--) {
-		if (!find()) {
-			cout << "0\n";
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			scanf(" %d", &arr[i][j]);
+	for(int i = 0; i < m; i++) {
+		if (!find(i)) {
+			printf("0\n");
 			return 0;
 		}
 	}
-	for (auto& p : v)
-		cout << p.val << " " << p.sy << " " << p.sx << " " << p.fy << " " << p.fx << '\n';
+	for (int i = 0; i < m; i++)
+		printf("%d %d %d %d %d\n", p[i].ret, p[i].sy + 1, p[i].sx + 1, p[i].fy + 1, p[i].fx + 1);
 	return 0;
 }
