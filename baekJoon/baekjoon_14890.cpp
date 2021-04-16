4ca6b8a7c454031d0077de7c116c01ca5d47374c
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-17 00:27:55 +0900

    [Gold 3] 경사로

--- baekJoon/baekjoon_14890.cpp
+++ baekJoon/baekjoon_14890.cpp
@@ -2,31 +2,28 @@
 
 #include <stdio.h>
 
-using namespace std;
+int n, L;
+int arr[101][201];
 
 int main() {
-	int n, l, res = 0;
-	int arr[101][202];
-	scanf("%d %d", &n, &l);
+	scanf("%d %d", &n, &L);
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			scanf(" %d", &arr[i][j]);
-
 	for (int j = 0; j < n; j++)
 		for (int i = 0; i < n; i++)
 			arr[j][i + n] = arr[i][j];
-
-	int i, cnt;
+	int ret = 0;
 	for (int j = 0; j < n + n; j++) {
-		cnt = 1;
+		int i, x = 1;
 		for (i = 0; i < n - 1; i++) {
-			if (arr[i][j] == arr[i + 1][j]) cnt++;
-			else if (arr[i][j] + 1 == arr[i + 1][j] && cnt >= l) cnt = 1;
-			else if (arr[i][j] - 1 == arr[i + 1][j] && cnt >= 0) cnt = 1 - l;
+			if (arr[i][j] == arr[i + 1][j]) x++;
+			else if (arr[i][j] + 1 == arr[i + 1][j] && x >= L) x = 1;
+			else if (arr[i][j] - 1 == arr[i + 1][j] && x >= 0) x = 1 - L;
 			else break;
 		}
-		if (i == n - 1 && cnt >= 0)	res++;
+		if (i == n - 1 && x >= 0) ret++;
 	}
-	printf("%d\n", res);
+	printf("%d\n", ret);
 	return 0;
 }
