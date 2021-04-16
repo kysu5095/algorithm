9cfbd39a7d212d858fc5174f1efeacf8393c0977
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-16 17:02:05 +0900

    [Silver 4] 퇴사

--- baekJoon/baekjoon_14501.cpp
+++ baekJoon/baekjoon_14501.cpp
@@ -1,24 +1,22 @@
 // 퇴사
 
 #include <stdio.h>
-#include <algorithm>
 
 int n, res = 0;
-int arr[15][2];
+int arr[16][2];
 
-void dfs(int day, int total) {
-	if (day >= n) {
-		res = res < total ? total : res;
-		return;
+void dfs(int idx, int total) {
+	res = res < total ? total : res;
+	for (int i = idx; i <= n; i++) {
+		if (arr[i][0] + i > n + 1) continue;
+		dfs(arr[i][0] + i, total + arr[i][1]);
 	}
-	if(day + arr[day][0] <= n) dfs(day + arr[day][0], total + arr[day][1]);
-	dfs(day + 1, total);
 }
 
 int main() {
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) scanf(" %d %d", &arr[i][0], &arr[i][1]);
-	dfs(0, 0);
+	for (int i = 1; i <= n; i++) scanf(" %d %d", &arr[i][0], &arr[i][1]);
+	dfs(1, 0);
 	printf("%d\n", res);
 	return 0;
 }
