281a8a75cde9e46bb2a4d373278202e975e0d39b
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-20 21:03:40 +0900

    [Gold 4] 드래곤 커브

--- baekJoon/baekjoon_15685.cpp
+++ baekJoon/baekjoon_15685.cpp
@@ -1,117 +1,44 @@
-// 드래곤 커브 //
-////////////////
+// 드래콘 커브
 
-#include <iostream>
-#include <stack>
+#include <stdio.h>
 #include <vector>
 
 using namespace std;
 
-int n, x, y, d, g;
-int cnt = 0;
-int arr[101][101] = { {0,}, };
-
-class Point {
-public:
-	int x, y, direct;
-};
-Point p[4] = { {1, 0}, {0, -1}, {-1, 0}, {0, 1} };
-
-void square() {
-	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 100; j++) {
-			if (arr[i][j] == 1 && arr[i][j + 1] == 1 && arr[i + 1][j] == 1 && arr[i + 1][j + 1] == 1) {
-				cnt++;
-			}
-		}
-	}
-}
-
-void dragon(stack<Point>& s) {
-	stack<Point> tmp = s;
-	int x = tmp.top().x;
-	int y = tmp.top().y;
-	int d = tmp.top().direct;
-
-	while (!tmp.empty()) {
-		d = (tmp.top().direct + 1) % 4;
-		x += p[d].x;
-		y += p[d].y;
-		s.push({ x, y, d });
-		tmp.pop();
-	}
-}
+int n;
+bool arr[101][101];
+int dy[4] = { 0, -1, 0, 1 };
+int dx[4] = { 1, 0, -1, 0 };
+vector<int> v;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> n;
+	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
-		stack<Point> s;
-		cin >> x >> y >> d >> g;
-		arr[y][x] = 1;
-		s.push({ x + p[d].x, y + p[d].y, d });
-		for (int j = 0; j < g; j++) {
-			dragon(s);
+		int y, x, d, g;
+		scanf(" %d %d %d %d", &x, &y, &d, &g);
+		arr[y][x] = true;
+		y += dy[d];
+		x += dx[d];
+		arr[y][x] = true;
+		v.push_back(d);
+		int len;
+		while (g--) {
+			len = v.size();
+			while (len--) {
+				d = (v[len] + 1) % 4;
+				y += dy[d];
+				x += dx[d];
+				arr[y][x] = true;
+				v.push_back(d);
+			}
 		}
-		while (!s.empty()) {
-			arr[s.top().y][s.top().x] = 1;
-			s.pop();
-		}
+		v.clear();
 	}
-	
-	square();
-	cout << cnt << '\n';
-
+	int ret = 0;
+	for (int i = 0; i < 100; i++)
+		for (int j = 0; j < 100; j++)
+			if (arr[i][j] && arr[i + 1][j] && arr[i + 1][j + 1] && arr[i][j + 1])
+				ret++;
+	printf("%d\n", ret);
 	return 0;
 }
-
-// #include <iostream>
-// #include <vector>
-
-// using namespace std;
-
-// int n;
-// bool arr[101][101] = { {false,}, };
-// int dy[4] = { 0, -1, 0, 1 };
-// int dx[4] = { 1, 0, -1, 0 };
-
-// int main() {
-// 	cin.sync_with_stdio(0);
-// 	cin.tie(0);
-// 	cin >> n;
-// 	int y, x, d, g;
-// 	int nd, len;
-// 	while (n--) {
-// 		cin >> x >> y >> d >> g;
-// 		arr[y][x] = true;
-// 		y += dy[d];
-// 		x += dx[d];
-// 		arr[y][x] = true;
-// 		vector<int> v;
-// 		v.push_back(d);
-// 		while (g--) {
-// 			len = v.size();
-// 			while (len--) {
-// 				d = (v[len] + 1) % 4;
-// 				y += dy[d];
-// 				x += dx[d];
-// 				arr[y][x] = true;
-// 				v.push_back(d);
-// 			}
-// 		}
-// 	}
-// 	int res = 0;
-// 	for (int i = 0; i < 100; i++) {
-// 		for (int j = 0; j < 100; j++) {
-// 			if (!arr[i][j]) continue;
-// 			if (!arr[i][j + 1]) continue;
-// 			if (!arr[i + 1][j + 1]) continue;
-// 			if (!arr[i + 1][j]) continue;
-// 			res++;
-// 		}
-// 	}
-// 	cout << res << '\n';
-// 	return 0;
-// }
