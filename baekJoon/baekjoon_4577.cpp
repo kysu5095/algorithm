54db06aa858ea674bd4a8ad77344f4c611715d29
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-04-22 21:05:58 +0900

    [Gold 2] 소코반

--- baekJoon/baekjoon_4577.cpp
+++ baekJoon/baekjoon_4577.cpp
@@ -1,116 +1,81 @@
 // 소코반
 
-#include <iostream>
+#include <stdio.h>
 #include <string.h>
 
-using namespace std;
+int r, c, box;
+char arr[17][17];
+int dy[4] = { 0, 0, 1, -1 };
+int dx[4] = { 1, -1, 0, 0 };
+char command[52];
 
-int r, c, rest;
-char arr[16][16], copy_arr[16][16];
-char command[51];
-int dy[4] = { -1, 1, 0, 0 };
-int dx[4] = { 0, 0, -1, 1 };
-
-void print() {
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			cout << arr[i][j];
-		}
-		cout << '\n';
-	}
-}
-
-int get_direct(char& d) {
-	switch (d) {
-	case 'U': return 0;
-	case 'D': return 1;
-	case 'L': return 2;
-	case 'R': return 3;
-	}
+int get_direction(char ch) {
+	if (ch == 'R') return 0;
+	else if (ch == 'L') return 1;
+	else if (ch == 'D') return 2;
+	else return 3;
 }
 
 bool game(int y, int x) {
-	int ny, nx, d, nny, nnx;
-	int len = strlen(command);
-	for (int idx = 0; idx < len; idx++) {
-		if (!rest) break;
-		d = get_direct(command[idx]);
-		ny = y + dy[d];
-		nx = x + dx[d];
+	for (int ch = 0; ch < strlen(command); ch++) {
+		int d = get_direction(command[ch]);
+		int ny = y + dy[d];
+		int nx = x + dx[d];
 		if (arr[ny][nx] == '#') continue;
-		if (arr[ny][nx] == '.' || arr[ny][nx] == '+') {
-			if (copy_arr[y][x] == '+') arr[y][x] = '+';
+		else if (arr[ny][nx] == '.') {
+			if (arr[y][x] == 'W') arr[y][x] = '+';
 			else arr[y][x] = '.';
-			y = ny;
-			x = nx;
-			if (arr[y][x] == '+') arr[y][x] = 'W';
-			else arr[y][x] = 'w';
+			arr[ny][nx] = 'w';
 		}
-		else {
-			nny = ny + dy[d];
-			nnx = nx + dx[d];
+		else if (arr[ny][nx] == '+') {
+			if (arr[y][x] == 'W') arr[y][x] = '+';
+			else arr[y][x] = '.';
+			arr[ny][nx] == 'W';
+		}
+		else if (arr[ny][nx] == 'b' || arr[ny][nx] == 'B') {
+			int nny = ny + dy[d];
+			int nnx = nx + dx[d];
 			if (arr[nny][nnx] == '#' || arr[nny][nnx] == 'b' || arr[nny][nnx] == 'B') continue;
-			if (copy_arr[y][x] == '+') arr[y][x] = '+';
+			
+			if (arr[y][x] == 'W') arr[y][x] = '+';
 			else arr[y][x] = '.';
-			if (arr[nny][nnx] == '+') {
-				arr[nny][nnx] = 'B';
-				rest--;
-			}
-			else arr[nny][nnx] = 'b';
-			y = ny;
-			x = nx;
-			if (copy_arr[y][x] == '+') {
-				arr[y][x] = 'W';
-				rest++;
-			}
-			else arr[y][x] = 'w';
-		}
-	}
-	for (int i = 0; i < r; i++)
-		for (int j = 0; j < c; j++)
-			if (arr[i][j] == '+') 
-				return false;
 
-	return true;
+			if (arr[nny][nnx] == '+') arr[nny][nnx] = 'B';
+			else arr[nny][nnx] = 'b';
+
+			if (arr[ny][nx] == 'B') arr[ny][nx] = 'W';
+			else arr[ny][nx] = 'w';
+
+			if (arr[ny][nx] == 'W' && arr[nny][nnx] == 'b') box++;
+			else if (arr[ny][nx] == 'w' && arr[nny][nnx] == 'B') box--;
+		}
+		y = ny;
+		x = nx;
+		if (box == 0) return true;
+	}
+	return false;
 }
 
 int main() {
-	cin.sync_with_stdio(0);
-	cin.tie(0);
-	int y, x, cnt = 0;
+	int cnt = 1;
 	while (true) {
-		cin >> r >> c;
+		int y, x;
+		scanf(" %d %d", &r, &c);
 		if (!r && !c) break;
-		cnt++;
-		rest = 0;
+		box = 0;
 		for (int i = 0; i < r; i++) {
-			cin >> arr[i];
+			scanf("%s", arr[i]);
 			for (int j = 0; j < c; j++) {
-				if (arr[i][j] == 'w') {
-					y = i;
-					x = j;
-					copy_arr[i][j] = '.';
-				}
-				else if (arr[i][j] == 'W') {
-					y = i;
-					x = j;
-					copy_arr[i][j] = '+';
-					rest++;
-				}
-				else if (arr[i][j] == 'B') {
-					copy_arr[i][j] = '+';
-				}
-				else if (arr[i][j] == '+') {
-					copy_arr[i][j] = '+';
-					rest++;
-				}
-				else copy_arr[i][j] = arr[i][j];
+				if (arr[i][j] == 'w' || arr[i][j] == 'W') y = i, x = j;
+				else if (arr[i][j] == 'b') box++;
 			}
 		}
-		cin >> command;
-		if (game(y, x)) cout << "Game " << cnt << ": complete\n";
-		else cout << "Game " << cnt << ": incomplete\n";
-		print();
+		scanf("%s", command);
+		printf("Game %d: ", cnt);
+		if (game(y, x)) printf("complete\n");
+		else printf("incomplete\n");
+		for (int i = 0; i < r; i++) printf("%s\n", arr[i]);
+		cnt++;
 	}
 	return 0;
 }
